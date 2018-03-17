1a893d920be70864759a776512803ab2f6ff3493
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-03-17 17:58:44 +0800

    添加爬虫测试代码，确定可行性

---
+++ crawler/crawlers/poj.js
@@ -0,0 +1,24 @@
+const request = require('superagent')
+const cheerio = require('cheerio')
+
+module.exports = function(config, username) {
+    return new Promise((resolve, reject) => {
+        request
+        .get('http://poj.org/userstatus')
+        .query({user_id: username})
+        .end((err, res) => {
+            if (err) {
+                reject(err)
+            } else if (!res.ok) {
+                reject(new Error(`Server Response Error: ${res.status}`))
+            }
+             {
+                const $ = cheerio.load(res.text)
+                resolve({
+                    solved: new Number($('a[href^="status?result=0&user_id="]').text()),
+                    submissions: new Number($('a[href^="status?user_id="]').text())
+                })
+            }
+        })
+    })
+}
