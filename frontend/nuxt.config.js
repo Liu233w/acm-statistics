48e5ad316f6ec38ffb6e05f4b70b013395af3062
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-03-19 14:34:15 +0800

    feat(前端): 添加 debug 插件，以便从浏览器命令行访问app组件

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -18,7 +18,13 @@ module.exports = {
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
-  plugins: ['~/plugins/vuetify.js'],
+  plugins: [
+    '~/plugins/vuetify.js',
+    {
+      src: "~/plugins/debug.js",
+      ssr: false
+    }
+  ],
   css: [
     '~/assets/style/app.styl'
   ],
---
+++ frontend/plugins/debug.js
@@ -0,0 +1,4 @@
+export default ({app}, inject) => {
+  // TODO: 改成 release 模式不使用
+  window.$app = app
+}
