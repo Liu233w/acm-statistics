059e643f05ab7b622db6197382880a5b8a8e0be0
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-03-24 23:58:41 +0800

    chore(爬虫API): 添加 eslint 支持

---
+++ crawler-api-backend/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: false,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 8,
+  },
+  extends: [
+    'eslint:recommended',
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:lodash/recommended',
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'lodash',
+  ],
+  // add your custom rules here
+  rules: {
+    'semi': [2, 'never'],
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
+    // 行末逗号：在多行中强制最后一项有逗号，单行中强制没有
+    'comma-dangle': ['error', 'always-multiline'],
+    'quotes': ['error', 'single'],
+    // 服务端不用管这个
+    'lodash/import-scope': 'off',
+    // 这个方法的可读性太低了
+    'lodash/matches-prop-shorthand': 'off',
+  },
+}
