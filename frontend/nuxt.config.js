3dd3a3223af5f84abddfde25f7ff7e741e9889e9
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-03-27 22:05:40 +0800

    perf(前端): 更换字体cdn地址
    由 https://sb.sb/blog/css-cdn/ 提供的谷歌字体反向代理，比直接连接速度更快。

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -16,7 +16,7 @@ module.exports = {
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'},
+      {rel: 'stylesheet', href: 'https://fonts.loli.net/css?family=Roboto:300,400,500,700|Material+Icons'},
     ],
   },
   plugins: [
