e9e8ecb1509bf6ecaa6e2e2b14a91a977b223838
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-04-11 00:57:08 +0800

    test(爬虫-configReader): 补充判断
    这样更精确一些

--- crawler/__test__/configReader.test.js
+++ crawler/__test__/configReader.test.js
@@ -1,11 +1,16 @@
 /* eslint-disable no-undef */
 
 const configReader = require('../lib/configReader')
+const _ = require('lodash')
 
 test('ensureConfigAndRead 能够正确读取配置', async () => {
   const config = await configReader.ensureConfigAndRead()
   expect(config.crawlers.length).not.toBe(0)
-  expect(config.crawlers[0].name).toBeTruthy()
+
+  const sguConfig = _.find(config.crawlers, item => item.name === 'sgu')
+  expect(sguConfig).toBeTruthy()
+  expect(sguConfig.meta).toBeTruthy()
+
 })
 
 test('readMetaConfigs 能够读取元信息', async () => {
