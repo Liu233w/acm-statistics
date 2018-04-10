3127dfec638ba1b41ac0925f9e5c93d24ae8635f
Liu233w <wwwlsmcom@outlook.com>
2018-04-10 17:27:46 +0800

    test(configReader): 更新测试，添加对 readMetaConfig 的测试

--- crawler/__test__/configReader.test.js
+++ crawler/__test__/configReader.test.js
@@ -1,8 +1,16 @@
-const {ensureConfigAndRead} = require("../lib/configReader")
 /* eslint-disable no-undef */
 
+const configReader = require('../lib/configReader')
+
 test('ensureConfigAndRead 能够正确读取配置', async () => {
-  const config = await ensureConfigAndRead()
+  const config = await configReader.ensureConfigAndRead()
   expect(config.crawlers.length).not.toBe(0)
   expect(config.crawlers[0].name).toBeTruthy()
+})
+
+test('readMetaConfigs 能够读取元信息', async () => {
+  const meta = await configReader.readMetaConfigs()
+  expect(meta.sgu).toBeTruthy()
+  expect(meta.sgu.title).toBe('SGU')
+  expect(meta.sgu.description).toBeTruthy()
 })
