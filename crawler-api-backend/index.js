7d30e6971131533c8393ce65c5a100e72fb6a429
Liu233w <wwwlsmcom@outlook.com>
2018-04-12 09:27:05 +0800

    refactor(爬虫API): 将 app 单独移动到一个文件中方便测试

--- crawler-api-backend/index.js
+++ crawler-api-backend/index.js
@@ -1,34 +1,3 @@
-const koa = require('koa')
-
-const restHelper = require('./utils/restHelper')
-const logUtil = require('./utils/logUtil')
-
-const app = new koa()
-const apiRouter = require('./apiRouter')
-
-const errHelper = async (ctx, next) => {
-  try {
-    await next()
-  } catch (err) {
-    ctx.error(err.message)
-    // 重新抛出异常，让它被上面的 logUtil 捕捉到
-    throw err
-  }
-}
-
-const notFoundHelper = async (ctx, next) => {
-  if (ctx.response.status === 404) {
-    ctx.error('404 Not Found')
-  }
-  await next()
-}
-
-app
-  .use(logUtil)
-  .use(restHelper)
-  .use(errHelper)
-  .use(apiRouter.routes())
-  .use(apiRouter.allowedMethods())
-  .use(notFoundHelper)
+const app = require('./app')
 
 app.listen(12001)
