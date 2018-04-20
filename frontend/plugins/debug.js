af4022507ee4cf886f3823abca4b10c0c3a183bd
Liu233w <wwwlsmcom@outlook.com>
2018-04-20 14:22:38 +0800

    feat(前端-Debug): 能够在 Debug 模式中访问 axios

--- frontend/plugins/debug.js
+++ frontend/plugins/debug.js
@@ -1,6 +1,9 @@
+import axios from 'axios'
+
 // eslint-disable-next-line no-unused-vars
 export default ({app}, inject) => {
   if (app.context.isDev && process.client) {
     window.$app = app
+    window.$axios = axios
   }
 }
