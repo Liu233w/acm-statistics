1e9ec60d7992b19dcef8c93b87c031917e6e35e4
Liu233w <wwwlsmcom@outlook.com>
2018-05-08 19:38:38 +0800

    test(前端): 给 ActionTester 添加功能
    如果 mutations 不存在，则不修改 state
    添加模拟 getters 的功能。

--- frontend/__test__/actionTester.js
+++ frontend/__test__/actionTester.js
@@ -1,10 +1,28 @@
 import _ from 'lodash'
 
 export default class ActionTester {
-  constructor(state = null, mutations = null) {
+  /**
+   * 初始化
+   * @param state {Object} mocked state
+   * @param mutations {Object.<string, {Function}>} mocked mutations, 如果存在，提交一个 mutations 的时候会调用
+   * @param getters {Object.<string, {Function|*}>} 如果为函数，得到的 getter 会使用此函数和 state 来计算出结果；否则直接以这个值作为结果
+   */
+  constructor(state = null, {mutations, getters} = {}) {
     this.commitHistory = []
     this.state = state
     this.mutations = mutations
+
+    this.getters = {}
+    if (getters) {
+      for (let key in getters) {
+        const val = getters[key]
+        if (_.isFunction(val)) {
+          Object.defineProperty(this.getters, key, {get: () => val(this.state)})
+        } else {
+          this.getters[key] = val
+        }
+      }
+    }
   }
 
   getCommitHistory() {
@@ -17,7 +35,13 @@ export default class ActionTester {
         type,
         payload: _.cloneDeep(payload),
       })
-      this.mutations[type](this.state, payload)
+      if (this.state && this.mutations) {
+        this.mutations[type](this.state, payload)
+      }
     }
+  }
+
+  getGetters() {
+    return this.getters
   }
 }
