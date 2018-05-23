4b207682f9d31e77598c2b6259b07acfb778c8b3
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-05-23 15:32:51 +0800

    perf(前端): 在 vuetify 输出css的时候进行压缩
    减小页面体积

--- frontend/plugins/vuetify.js
+++ frontend/plugins/vuetify.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import LRU from 'lru-cache'
 
 import Vue from 'vue'
@@ -47,6 +48,12 @@ Vue.use(Vuetify, {
     VChip,
   },
   options: {
+    minifyTheme: function (val) {
+      if (process.env.NODE_ENV === 'production') {
+        val = _.replace(val, /[\s|\r\n|\r|\n]/g, '')
+      }
+      return val
+    },
     themeCache: LRU({
       max: 10,
       maxAge: 1000 * 60 * 60 * 12, // 12 hour
