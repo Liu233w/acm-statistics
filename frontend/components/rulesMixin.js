e92a8e1c00373119abdc4b6ee26b7442c8c6e0ed
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-05-27 17:26:51 +0800

    feat(前端): 完成登录页面样式

---
+++ frontend/components/rulesMixin.js
@@ -0,0 +1,13 @@
+export default {
+  data() {
+    return {
+      rules: {
+        required: (value) => !!value || 'Required.',
+        email: (value) => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid e-mail.'
+        },
+      },
+    }
+  },
+}
---
+++ frontend/pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card flat>
+    <v-card-title primary-title>
+      <div class="headline">登录</div>
+    </v-card-title>
+    <v-container>
+      <v-layout row>
+        <v-flex>
+          <v-form v-model="valid" lazy-validation>
+            <v-text-field prepend-icon="person" label="Username Or Email" required v-model="username" :rules="[rules.required]"/>
+            <v-text-field prepend-icon="lock" label="Password" required type="password" v-model="password" :rules="[rules.required]"/>
+            <v-card-actions>
+              <v-btn color="info" block :disabled="!valid">登录</v-btn>
+              <v-btn block to="/register">去注册</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+  import rulesMixin from '~/components/rulesMixin'
+
+  export default {
+    layout: 'login',
+    mixins: [rulesMixin],
+    data() {
+      return {
+        username: '',
+        password: '',
+        valid: false,
+      }
+    },
+  }
+</script>
