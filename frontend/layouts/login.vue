b714cc597ef623b94f6d6a884401d6ca8fe28c7f
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-05-27 17:26:51 +0800

    feat(前端): 完成登录页面样式

---
+++ frontend/layouts/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app light>
+    <v-container align-content-center>
+      <v-layout child-flex row class="text-xs-center">
+        <v-spacer/>
+        <v-flex xs3>
+          <v-card height="500px" class="background"/>
+        </v-flex>
+        <v-flex xs4 d-flex class="grey lighten-4">
+          <v-spacer/>
+          <v-container class="text-xs-center">
+            <nuxt/>
+          </v-container>
+          <v-spacer/>
+        </v-flex>
+        <v-spacer/>
+      </v-layout>
+    </v-container>
+  </v-app>
+</template>
+
+<style scoped>
+  .background {
+    background-image: url('~/assets/img/login/background.jpg')
+  }
+
+  .vertical-center {
+  }
+</style>
---
+++ frontend/pages/register.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card flat>
+    <v-card-title primary-title>
+      <div class="headline">注册</div>
+    </v-card-title>
+    <v-container>
+      <v-layout row>
+        <v-flex>
+          <v-form v-model="valid" lazy-validation>
+            <v-text-field prepend-icon="person" label="Username" v-model="username" required :rules="[rules.required]"/>
+            <v-text-field prepend-icon="email" label="Email" v-model="email" required
+                          :rules="[rules.required, rules.email]"/>
+            <v-text-field prepend-icon="lock" label="Password" type="password" v-model="password" required
+                          :rules="[rules.required]"/>
+            <v-text-field prepend-icon="lock" label="Password Again" type="password" v-model="pwdRepeat" required
+                          :rules="[rules.required, () => password === pwdRepeat || '两次密码必须相同']"/>
+            <v-card-actions>
+              <v-btn color="info" block :disabled="!valid">注册</v-btn>
+              <v-btn block to="/login">去登录</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+  import rulesMixin from '~/components/rulesMixin'
+
+  export default {
+    layout: 'login',
+    mixins: [rulesMixin],
+    data() {
+      return {
+        username: '',
+        email: '',
+        password: '',
+        pwdRepeat: '',
+        valid: false,
+      }
+    },
+    methods: {
+      // submit() {
+      //   return null
+      // },
+    },
+  }
+</script>
