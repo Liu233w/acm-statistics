71578a71136ed58e45b2b89829d1a4853efb09da
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-12-30 21:15:54 +0800

    chore(tools): 更新 docker-shell 运行脚本
    默认暴露 3000 端口，便于调试

--- tools/docker-shell.cmd
+++ tools/docker-shell.cmd
@@ -1,1 +1,1 @@
-docker run --rm -it --workdir /var/src/project/ --volume "%~dp0..:/var/src/project" node:10-alpine /usr/bin/env sh
+docker run --rm -it --workdir /var/src/project/ --volume "%~dp0..:/var/src/project" -p 3000:3000 --env HOST=0.0.0.0 node:10-alpine /usr/bin/env sh
