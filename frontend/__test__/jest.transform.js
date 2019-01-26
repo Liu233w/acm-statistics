1cda89b874f3d2e78ee3cf2c565ae500194ab267
Liu233w <wwwlsmcom@outlook.com>
2019-01-26 17:44:06 +0800

    test(前端): 只将 import 转换成 node 可以识别的形式，不转换其他代码

--- frontend/__test__/jest.transform.js
+++ frontend/__test__/jest.transform.js
@@ -1,4 +1,6 @@
 // 用于将 es6 格式的 module 转换成 node 能识别的格式
 module.exports = require('babel-jest').createTransformer({
-  presets: ['node8'],
+  plugins: [
+    'transform-es2015-modules-commonjs',
+  ],
 })
