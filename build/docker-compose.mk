17c115a7f35ceed860ceea0fc0955588cd90f9d5
Liu233w <wwwlsmcom@outlook.com>
2019-02-09 18:35:52 +0800

    Revert "build(makefile): 移除 dev 模式的 docker compose 的所有代码"
    This reverts commit e3c22be65b1959e38a6461245310fbd512d0fb2f.

--- build/docker-compose.mk
+++ build/docker-compose.mk
@@ -28,6 +28,11 @@ $(ImageToTag) $(ImageToPush): ImageNameWithLatest = $(RepoName)$(Image):latest
 	$(MAKE) -C ../crawler-api-backend build
 	$(MAKE) -C ../frontend build
 
+# 为了开发而构建的目标
+.build-dev:
+	$(MAKE) -C ../crawler-api-backend build
+	# 前端直接映射本地的目录，不需要构建
+
 tag: .build $(ImageToTag)
 
 push: tag $(ImageToPush)
@@ -47,3 +52,6 @@ up: .build .env
 
 .env:
 	cp template.env .env
+
+dev: .build-dev
+	docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
