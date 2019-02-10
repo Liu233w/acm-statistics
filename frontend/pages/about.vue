27b493fe30b7f17c4cdbc6d876b63d024bce3955
Liu233w <wwwlsmcom@outlook.com>
2019-02-10 16:13:07 +0800

    style(frontend): 根据最新的 eslint 规则修复页面

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,42 +1,42 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12 sm6 offset-sm3>
-        <v-card>
-          <v-toolbar color="light-blue" dark>
-            <v-toolbar-title>关于</v-toolbar-title>
-            <v-spacer />
-          </v-toolbar>
-          <v-list>
-            <v-list-tile>
-              <v-list-tile-content>
-                <v-list-tile-title>版本号</v-list-tile-title>
-                <v-list-tile-sub-title>{{ $env.VERSION_NUM }}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
+  <VContainer>
+    <VLayout row>
+      <VFlex xs12 sm6 offset-sm3>
+        <VCard>
+          <VToolbar color="light-blue" dark>
+            <VToolbarTitle>关于</VToolbarTitle>
+            <VSpacer />
+          </VToolbar>
+          <VList>
+            <VListTile>
+              <VListTileContent>
+                <VListTileTitle>版本号</VListTileTitle>
+                <VListTileSubTitle>{{ $env.VERSION_NUM }}</VListTileSubTitle>
+              </VListTileContent>
+            </VListTile>
 
-            <v-list-tile :href="`https://github.com/Liu233w/acm-statistics/tree/${$env.VERSION_NUM}`">
-              <v-list-tile-content>
-                <v-list-tile-title>当前版本源代码</v-list-tile-title>
-                <v-list-tile-sub-title>
+            <VListTile :href="`https://github.com/Liu233w/acm-statistics/tree/${$env.VERSION_NUM}`">
+              <VListTileContent>
+                <VListTileTitle>当前版本源代码</VListTileTitle>
+                <VListTileSubTitle>
                   https://github.com/Liu233w/acm-statistics/tree/{{ $env.VERSION_NUM }}
-                </v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
+                </VListTileSubTitle>
+              </VListTileContent>
+            </VListTile>
 
-            <v-list-tile :href="`https://github.com/Liu233w/acm-statistics/commit/${$env.VERSION_NUM}`">
-              <v-list-tile-content>
-                <v-list-tile-title>最近更新</v-list-tile-title>
-                <v-list-tile-sub-title>
+            <VListTile :href="`https://github.com/Liu233w/acm-statistics/commit/${$env.VERSION_NUM}`">
+              <VListTileContent>
+                <VListTileTitle>最近更新</VListTileTitle>
+                <VListTileSubTitle>
                   https://github.com/Liu233w/acm-statistics/commit/{{ $env.VERSION_NUM }}
-                </v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+                </VListTileSubTitle>
+              </VListTileContent>
+            </VListTile>
+          </VList>
+        </VCard>
+      </VFlex>
+    </VLayout>
+  </VContainer>
 </template>
 
 <script>
