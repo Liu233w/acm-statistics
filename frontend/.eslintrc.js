cdbfb6b325cae5ffd02a710661c4d0a530e165fd
Liu233w <wwwlsmcom@outlook.com>
2019-02-10 16:02:43 +0800

    style(eslint): 修复配置文件的代码格式

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,18 +3,18 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
-    "eslint:recommended",
+    'eslint:recommended',
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    "plugin:vue/strongly-recommended",
+    'plugin:vue/strongly-recommended',
     'plugin:lodash/recommended',
-    "plugin:jest/recommended",
+    'plugin:jest/recommended',
   ],
   // required to lint *.vue files
   plugins: [
@@ -24,16 +24,16 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    "semi": [2, "never"],
-    "no-console": "off",
-    "vue/max-attributes-per-line": "off",
+    'semi': [2, 'never'],
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
     // 行末逗号：在多行中强制最后一项有逗号，单行中强制没有
-    "comma-dangle": ["error", "always-multiline"],
-    "quotes": ["error", "single"],
+    'comma-dangle': ['error', 'always-multiline'],
+    'quotes': ['error', 'single'],
     // 有 babel-lodash，不需要单独引入也可以 tree-shaking
-    "lodash/import-scope": "off",
+    'lodash/import-scope': 'off',
     // 这个方法的可读性太低了
-    'lodash/matches-prop-shorthand': "off",
+    'lodash/matches-prop-shorthand': 'off',
     'lodash/prefer-reject': 'off',
     // 强制使用 PascalCase
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
