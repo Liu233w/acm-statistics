1b95628ed2e6ed620ec4e007a5b8e95ca8419937
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-03-13 22:47:22 +0800

    test(crawler-api-backend): 修复测试代码
    现在 supertest 的 await 会在返回值为 4XX 和 5XX 时抛出异常了，为了代码简短起见，就使用 supertest
    自带的断言代码了。

--- crawler-api-backend/__test__/apiRouter.test.js
+++ crawler-api-backend/__test__/apiRouter.test.js
@@ -18,68 +18,68 @@ afterAll(() => {
 
 test('/api/crawlers/swagger.json swagger should match snapshot', async () => {
   const res = await request.get('/api/crawlers/swagger.json')
-  expect(res.status).toBe(200)
+    .expect(200)
   expect(res.body).toMatchSnapshot()
 })
 
-test('/api/crawlers should return crawler list', async () => {
-  const res = await request.get('/api/crawlers')
-  expect(res.status).toBe(200)
-  expect(res.body).toMatchObject({
-    error: false,
-    data: {
-      crawler1: {
-        title: 'Crawler1',
-        description: 'Description1',
-        url: 'http://www.c1.com',
+test('/api/crawlers should return crawler list', done => {
+  request
+    .get('/api/crawlers')
+    .expect(200, {
+      error: false,
+      data: {
+        crawler1: {
+          title: 'Crawler1',
+          description: 'Description1',
+          url: 'http://www.c1.com',
+        },
+        crawler2: {
+          title: 'Crawler2',
+        },
+        crawler_for_server: {
+          title: 'CrawlerForServer',
+        },
       },
-      crawler2: {
-        title: 'Crawler2',
-      },
-      crawler_for_server: {
-        title: 'CrawlerForServer',
-      },
-    },
-  })
+    }, done)
 })
 
 describe('/api/crawlers/:type/:username', () => {
-  it('在结果正确时应该返回题量', async () => {
-    const res = await request.get('/api/crawlers/crawler1/user')
-    expect(res.status).toBe(200)
-    expect(res.body).toMatchObject({
-      error: false,
-      data: {
-        solved: 101,
-        submissions: 230,
-      },
-    })
+  it('在结果正确时应该返回题量', done => {
+    request
+      .get('/api/crawlers/crawler1/user')
+      .expect(200, {
+        error: false,
+        data: {
+          solved: 101,
+          submissions: 230,
+        },
+      }, done)
   })
 
-  it('在爬虫不存在时应该返回 400', async () => {
-    const res = await request.get('/api/crawlers/notExist/user')
-    expect(res.status).toBe(400)
-    expect(res.body).toMatchObject({
-      error: true,
-      message: '不存在此OJ的爬虫',
-    })
+  it('在爬虫不存在时应该返回 400', done => {
+    request
+      .get('/api/crawlers/notExist/user')
+      .expect(400, {
+        error: true,
+        message: '不存在此OJ的爬虫',
+      }, done)
   })
 
-  it('在爬虫报错时应该返回错误结果', async () => {
-    const res = await request.get('/api/crawlers/crawler1/reject')
-    expect(res.status).toBe(400)
-    expect(res.body).toMatchObject({
-      error: true,
-      message: '用户不存在',
-    })
+  it('在爬虫报错时应该返回错误结果', done => {
+    request
+      .get('/api/crawlers/crawler1/reject')
+      .expect(400, {
+        error: true,
+        message: '用户不存在',
+      }, done)
   })
 })
 
-test('在访问不存在的资源时返回 404', async () => {
-  const res = await request.get('/notExists')
-  expect(res.status).toBe(404)
-  expect(res.body).toMatchObject({
-    error: true,
-    message: '404 Not Found',
-  })
+test('在访问不存在的资源时返回 404', done => {
+  request
+    .get('/notExists')
+    .expect(404, {
+      error: true,
+      message: '404 Not Found',
+    }, done)
 })
