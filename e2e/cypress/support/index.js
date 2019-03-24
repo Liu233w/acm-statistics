33dc9d3dcbd9b4bc034cd7c8a379afa55c0432a9
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-03-24 17:31:00 +0800

    test(e2e): 添加 image-snapshot

--- e2e/cypress/support/index.js
+++ e2e/cypress/support/index.js
@@ -15,6 +15,9 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import {addMatchImageSnapshotCommand} from 'cypress-image-snapshot/command'
+
+addMatchImageSnapshotCommand()
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
