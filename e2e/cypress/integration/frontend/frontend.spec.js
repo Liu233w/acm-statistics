1418a0ade836dc3336821f29fceae4ae0063fd40
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-03-31 15:49:33 +0800

    test(e2e): 添加首页的快照测试

---
+++ e2e/cypress/integration/frontend/frontend.spec.js
@@ -0,0 +1,29 @@
+describe('/', () => {
+
+  beforeEach(() => {
+    cy.mockServer('busuanzi')
+  })
+
+  it('能够正确渲染', () => {
+    cy.visit('/')
+    cy.matchImageSnapshot()
+  })
+
+  describe('能够在不同尺寸的手持设备下正确渲染', () => {
+    [
+      'ipad-2',
+      'ipad-mini',
+      'iphone-6+',
+      'iphone-6',
+      'iphone-5',
+    ].forEach(item => {
+
+      it(item.toString(), () => {
+        cy.viewport(item)
+        cy.visit('/')
+        cy.matchImageSnapshot()
+      })
+    })
+  })
+
+})
