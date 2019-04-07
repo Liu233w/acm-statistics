242d333ae7bc9a6e00a21062da0be77abad0706c
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-04-07 17:07:13 +0800

    chore(frontend): 在 debug 模式下打开性能分析

--- frontend/plugins/debug.js
+++ frontend/plugins/debug.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
+import Vue from 'vue'
 
 // eslint-disable-next-line no-unused-vars
 export default ({app}, inject) => {
   if (app.context.isDev && process.client) {
     window.$app = app
     window.$axios = axios
+
+    // 打开性能分析
+    Vue.config.performance = true
   }
 }
