a76951cfeee338c9fcc715529e1eaaf76efb9138
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-06-14 22:33:30 +0800

    test(e2e): 移除 font-awesome 的 mock

--- e2e/http-mocks/src/mocks/travis.js
+++ e2e/http-mocks/src/mocks/travis.js
@@ -1,22 +1,4 @@
 module.exports = {
-  mock_font_awesome(client) {
-    return client.mockAnyResponse({
-      httpRequest: {
-        path: '/font-awesome/.*',
-        headers: {
-          host: ['cdn.bootcss.com'],
-        },
-      },
-      httpForward: {
-        'host': 'stackpath.bootstrapcdn.com',
-        'port': 80,
-        'scheme': 'HTTP',
-      },
-      times: {
-        unlimited: true,
-      },
-    })
-  },
   mock_font_cdn(client) {
     return client.mockAnyResponse({
       httpRequest: {
