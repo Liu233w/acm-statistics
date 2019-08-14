14d42bef1b4064f950d8c0176badf62c76126167
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-08-14 20:17:08 +0800

    fix(crawler): 修复 leetcode_cn
    直接使用graphql来查询

--- crawler/crawlers/leetcode_cn.js
+++ crawler/crawlers/leetcode_cn.js
@@ -1,5 +1,4 @@
 const request = require('superagent')
-const cheerio = require('cheerio')
 
 module.exports = async function (config, username) {
 
@@ -7,28 +6,23 @@ module.exports = async function (config, username) {
     throw new Error('请输入用户名')
   }
 
-  let res
-  try {
-    res = await request
-      .get('https://leetcode-cn.com/' + username)
-  } catch (e) {
-    if (e.response && e.response.status == 404) {
-      throw new Error('用户不存在')
-    } else {
-      throw e
-    }
+  const input = {
+    query: 'query userPublicProfile($userSlug:String!){userProfilePublicProfile(userSlug:$userSlug){submissionProgress{totalSubmissions acTotal}}}',
+    variables: { userSlug: username },
   }
 
-  const $ = cheerio.load(res.text)
+  const res = await request
+    .post('https://leetcode-cn.com/graphql')
+    .send(input)
 
-  try {
-    const solvedElem = $('li.list-group-item:contains("解决的题目") > span')
-    const submissionsElem = $('li.list-group-item:contains("通过的提交") > span')
-    return {
-      solved: Number($(solvedElem).text().replace(/[ \n]/g, '').split('/')[0]),
-      submissions: Number($(submissionsElem).text().replace(/[ \n]/g, '').split('/')[1]),
-    }
-  } catch (e) {
-    throw new Error('无法解析数据')
+  const data = res.body.data.userProfilePublicProfile
+
+  if (!data) {
+    throw new Error('用户不存在')
+  }
+
+  return {
+    solved: data.submissionProgress.acTotal,
+    submissions: data.submissionProgress.totalSubmissions,
   }
 }
