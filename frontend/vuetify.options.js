18d8aacb5f682aee8c402c3a5a963aa6282e31b8
Liu's DeskTop <wwwlsmcom@msn.cn>
2019-08-22 18:06:54 +0800

    refactor(frontend): 完成 vuetify v2 的配置升级
    还剩下迁移没有做

---
+++ frontend/vuetify.options.js
@@ -0,0 +1,24 @@
+import _ from 'lodash'
+import LRU from 'lru-cache'
+
+import '@fortawesome/fontawesome-free/css/all.css'
+
+export default {
+  theme: {
+    options: {
+      minifyTheme: function (val) {
+        if (process.env.NODE_ENV === 'production') {
+          val = _.replace(val, /[\s|\r\n|\r|\n]/g, '')
+        }
+        return val
+      },
+      themeCache: new LRU({
+        max: 10,
+        maxAge: 1000 * 60 * 60 * 12, // 12 hours
+      }),
+    },
+  },
+  icons: {
+    iconfont: 'fa',
+  }
+}
