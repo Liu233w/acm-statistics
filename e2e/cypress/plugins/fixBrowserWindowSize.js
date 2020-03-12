144b990d57a2b4052b60b168e28ed476afb26a58
Liu233w <wwwlsmcom@outlook.com>
2020-03-12 20:20:06 +1100

    test(e2e): update plugin
    use api of cypress 4.0

--- e2e/cypress/plugins/fixBrowserWindowSize.js
+++ e2e/cypress/plugins/fixBrowserWindowSize.js
@@ -3,21 +3,17 @@ from https://github.com/cypress-io/cypress/issues/495#issuecomment-472723434
  */
 
 module.exports = (on) => {
-  on('before:browser:launch', (browser = {}, args) => {
+  on('before:browser:launch', (browser = {}, options) => {
+
+    const args = options.args
+
     if (browser.name === 'chrome') {
       args.push('--window-size=200,800')
-
-      // whatever you return here becomes the new args
-      return args
     }
-
-    if (browser.name === 'electron') {
+    else if (browser.name === 'electron') {
       args['width'] = 1600
       args['height'] = 900
       args['resizable'] = false
-
-      // whatever you return here becomes the new args
-      return args
     }
   })
 }
