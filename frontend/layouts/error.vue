9dc4b8f350c883440447bf65ec0fde16b299b955
Liu233w <wwwlsmcom@outlook.com>
2020-03-27 19:56:17 +1100

    feat(frontend): 添加错误页面

---
+++ frontend/layouts/error.vue
@@ -0,0 +1,71 @@
+<template>
+  <VApp light>
+    <v-container class="fill-height">
+      <v-row
+        class="mt-n12"
+        justify="center"
+      >
+        <v-col
+          class="text-center"
+          cols="auto"
+        >
+          <v-img
+            class="mb-12"
+            contain
+            height="256"
+            :src="randomMeme()"
+          />
+          <h1 class="display-4 font-weight-bold mb-6">
+            ???
+          </h1>
+
+          <p class="mb-12 display-1 grey--text text--darken-2">
+            {{ message }}
+          </p>
+
+          <v-btn
+            color="primary"
+            x-large
+            to="/"
+          >
+            返回主页
+
+            <v-icon right>
+              mdi-open-in-new
+            </v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </VApp>
+</template>
+
+<script>
+import _ from 'lodash'
+
+const memes = [
+  '/img/error/cat.jpg',
+  '/img/error/metro.jpg',
+  '/img/error/man.jpg',
+]
+
+export default {
+   props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    message () {
+      return this.error.message || '????????'
+    },
+  },
+  layout: 'none',
+  methods: {
+    randomMeme() {
+      return memes[_.random(memes.length-1)]
+    },
+  },
+}
+</script>
