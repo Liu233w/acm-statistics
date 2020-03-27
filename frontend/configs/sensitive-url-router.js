4639167de8ca39b75136d48992f40fef0fac9d94
Liu233w <wwwlsmcom@outlook.com>
2020-03-27 20:26:22 +1100

    feat(frontend): 给试图探测的人一点惊喜
    resolve #95

---
+++ frontend/configs/sensitive-url-router.js
@@ -0,0 +1,49 @@
+const { sortRoutes } = require('@nuxt/utils')
+
+const sensitiveRoutes = [
+  '/index.html',
+  '/index.php',
+  '/index.jsp',
+  '/admin/',
+  '/wp-login.php',
+  '/readme.html',
+  '/license.txt',
+  '/wp-includes/js/wplink.js',
+  '/wp-admin/js/customize-controls.js',
+  '/wp-admin/js/nav-menu.js',
+  '/wp-includes/js/plupload',
+  '/wp-includes/js/tinymce/',
+  '/README',
+  '/phpMyAdmin/',
+  '/phpmyadmin/',
+  '/pma/',
+  '/solr/',
+  '/wcm/',
+  '/swagger/elpsycongroo',
+  '/ZeroClipboard.swf',
+  '/js/ZeroClipboard.swf',
+  '/script/ZeroClipboard.swf',
+  '/lib/ZeroClipboard.swf',
+  '/api.php',
+  '/checktable.php',
+  '/theme/default/images/kindeditor/save.gif',
+  '/js/kindeditor/Makefile',
+  '/theme/default/images/treeview/file.gif',
+  '/js/jquery/treeview/min.js',
+  '/theme/default/images/main/logo.png',
+  '/js/jquery/syntaxhighlighter/scripts/shBrushPlain.js',
+  '/theme/default/style.css',
+  '/js/my.full.js',
+  '/theme/zui/fonts/zenicon.eot',
+]
+
+module.exports = function (routes, resolve) {
+  for (let route of sensitiveRoutes) {
+    routes.push({
+      path: route,
+      component: resolve(__dirname, '../pages/jojo.vue'),
+    })
+  }
+
+  sortRoutes(routes)
+}
---
+++ frontend/pages/jojo.vue
@@ -0,0 +1,36 @@
+<template>
+  <VApp light>
+    <v-container class="fill-height">
+      <v-row
+        class="mt-n12"
+        justify="center"
+      >
+        <v-col
+          class="text-center"
+          cols="auto"
+        >
+          <v-img
+            class="mb-12"
+            contain
+            height="256"
+            :src="require('~/assets/img/dio.jpg')"
+          />
+          <h1 class="display-2 font-weight-bold mb-6">
+            你以为是 {{ url }}，其实是我DIO哒!!
+          </h1>
+        </v-col>
+      </v-row>
+    </v-container>
+  </VApp>
+</template>
+
+<script>
+export default {
+  computed: {
+    url() {
+      return this.$nuxt.$route.path
+    },
+  },
+  layout: 'none',
+}
+</script>
