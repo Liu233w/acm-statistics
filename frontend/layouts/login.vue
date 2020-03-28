44927fb353d96ca065b8d5799177341c1fc58c3e
Liu233w <wwwlsmcom@outlook.com>
2020-03-28 18:00:45 +1100

    style(frontend): 修正缩进

--- frontend/layouts/login.vue
+++ frontend/layouts/login.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app light>
     <v-container align-content-center>
-      <v-layout child-flex row class="text-xs-center">
-        <v-spacer/>
+      <v-layout child-flex class="text-center">
+        <v-spacer />
         <v-flex xs3>
-          <v-card height="500px" class="background"/>
+          <v-card height="500px" class="background" />
         </v-flex>
         <v-flex xs4 d-flex class="grey lighten-4">
-          <v-spacer/>
-          <v-container class="text-xs-center">
-            <nuxt/>
+          <v-spacer />
+          <v-container class="text-center">
+            <nuxt />
           </v-container>
-          <v-spacer/>
+          <v-spacer />
         </v-flex>
-        <v-spacer/>
+        <v-spacer />
       </v-layout>
     </v-container>
   </v-app>
