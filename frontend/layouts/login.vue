057e7f538e6944fa143282d3d47d0dc2863cf050
Liu233w <wwwlsmcom@outlook.com>
2020-03-28 19:02:04 +1100

    feat(frontend): 完成登录、注册页面的样式

--- frontend/layouts/login.vue
+++ frontend/layouts/login.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app light>
-    <v-container align-content-center>
-      <v-layout child-flex class="text-center">
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
         <v-spacer />
         <v-flex xs3>
           <v-card height="500px" class="background" />
@@ -14,16 +14,13 @@
           <v-spacer />
         </v-flex>
         <v-spacer />
-      </v-layout>
+      </v-row>
     </v-container>
   </v-app>
 </template>
 
 <style scoped>
-  .background {
-    background-image: url('~assets/img/login/background.jpg')
-  }
-
-  .vertical-center {
-  }
+.background {
+  background-image: url("~assets/img/login/background.jpg");
+}
 </style>
--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -9,16 +9,33 @@
       <v-layout>
         <v-flex>
           <v-form v-model="valid" lazy-validation>
-            <v-text-field prepend-icon="person" label="Username" required v-model="username" :rules="[rules.required]" />
-            <v-text-field prepend-icon="lock" label="Password" required type="password" v-model="password" :rules="[rules.required]" />
-            <v-card-actions>
-              <v-btn color="info" block :disabled="!valid">
-                登录
-              </v-btn>
-              <v-btn block to="/register">
-                去注册
-              </v-btn>
-            </v-card-actions>
+            <v-text-field
+              prepend-icon="person"
+              label="用户名"
+              required
+              v-model="username"
+              :rules="[rules.required]"
+            />
+            <v-text-field
+              prepend-icon="lock"
+              label="密码"
+              required
+              type="password"
+              v-model="password"
+              :rules="[rules.required]"
+            />
+            <v-row>
+              <v-col>
+                <v-btn color="info" block :disabled="!valid">
+                  登录
+                </v-btn>
+              </v-col>
+              <v-col>
+                <v-btn block to="/register">
+                  去注册
+                </v-btn>
+              </v-col>
+            </v-row>
           </v-form>
         </v-flex>
       </v-layout>
@@ -27,17 +44,17 @@
 </template>
 
 <script>
-  import rulesMixin from '~/components/rulesMixin'
+import rulesMixin from '~/components/rulesMixin'
 
-  export default {
-    layout: 'login',
-    mixins: [rulesMixin],
-    data() {
-      return {
-        username: '',
-        password: '',
-        valid: false,
-      }
-    },
-  }
+export default {
+  layout: 'login',
+  mixins: [rulesMixin],
+  data() {
+    return {
+      username: '',
+      password: '',
+      valid: false,
+    }
+  },
+}
 </script>
