0be15f765764d52fe3f5f7de47c613b7e24eb076
Liu233w <wwwlsmcom@outlook.com>
2020-03-28 20:47:50 +1100

    refactor(backend): 移除 IsTenantAvailable
    不需要这个功能

--- backend/src/AcmStatisticsBackend.Application/Authorization/Accounts/IAccountAppService.cs
+++ backend/src/AcmStatisticsBackend.Application/Authorization/Accounts/IAccountAppService.cs
@@ -6,8 +6,6 @@ namespace AcmStatisticsBackend.Authorization.Accounts
 {
     public interface IAccountAppService : IApplicationService
     {
-        Task<IsTenantAvailableOutput> IsTenantAvailable(IsTenantAvailableInput input);
-
         Task<RegisterOutput> Register(RegisterInput input);
     }
 }
