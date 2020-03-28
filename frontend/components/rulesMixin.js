ab976abf847845dbdb0900ef4630717b469dfc40
Liu233w <wwwlsmcom@outlook.com>
2020-03-28 19:02:04 +1100

    feat(frontend): 完成登录、注册页面的样式

--- frontend/components/rulesMixin.js
+++ frontend/components/rulesMixin.js
@@ -2,10 +2,10 @@ export default {
   data() {
     return {
       rules: {
-        required: (value) => !!value || 'Required.',
+        required: (value) => !!value || '必须输入',
         email: (value) => {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || 'Invalid e-mail.'
+          return pattern.test(value) || '需要格式正确的邮箱地址'
         },
       },
     }
