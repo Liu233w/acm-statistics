804879bcb70261c8bd2686c74f80fc0ca681ebd2
Liu233w <wwwlsmcom@outlook.com>
2020-03-29 01:12:31 +1100

    feat(frontend): 部分添加登录注册功能
    未调试

--- frontend/components/utils.js
+++ frontend/components/utils.js
@@ -6,3 +6,18 @@
 export function getDateFromTimestamp(timestamp) {
   return new Date(parseInt(timestamp) * 1000)
 }
+
+/**
+ * 从axios访问abp的异常中找出错误信息
+ * @param {Error} err axios抛出的异常
+ */
+export function getAbpErrorMessage(err) {
+  if (!err.response.data) {
+    return {
+      message: '网络错误',
+      details: err.message,
+    }
+  } else {
+    return err.response.data.error
+  }
+}
