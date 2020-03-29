8d9fb50f6cf3ee13e98c6b6dc7aae5375be652f0
Liu233w <wwwlsmcom@outlook.com>
2020-03-29 20:24:18 +1100

    fix(backend): 修复注册时邮箱名相同的问题

--- backend/src/AcmStatisticsBackend.Core/AcmStatisticsBackendConsts.cs
+++ backend/src/AcmStatisticsBackend.Core/AcmStatisticsBackendConsts.cs
@@ -7,5 +7,10 @@ namespace AcmStatisticsBackend
         public const string ConnectionStringName = "Default";
 
         public const bool MultiTenancyEnabled = false;
+
+        /// <summary>
+        /// 用户没有输入邮箱时，使用这个后缀作为邮箱名
+        /// </summary>
+        public const string NoEmailSuffix = "@noemail.fake";
     }
 }
