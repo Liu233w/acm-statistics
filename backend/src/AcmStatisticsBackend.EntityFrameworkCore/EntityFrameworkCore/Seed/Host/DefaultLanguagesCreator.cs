252240bceb4f603bf77ff3dfbab6a47b4afefc0e
Liu233w <wwwlsmcom@outlook.com>
2020-03-30 17:54:01 +1100

    perf(backend): 移除多余的语言
    后端默认只返回中文

--- backend/src/AcmStatisticsBackend.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefaultLanguagesCreator.cs
+++ backend/src/AcmStatisticsBackend.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefaultLanguagesCreator.cs
@@ -17,18 +17,7 @@ namespace AcmStatisticsBackend.EntityFrameworkCore.Seed.Host
             var tenantId = AcmStatisticsBackendConsts.MultiTenancyEnabled ? null : (int?)MultiTenancyConsts.DefaultTenantId;
             return new List<ApplicationLanguage>
             {
-                new ApplicationLanguage(tenantId, "en", "English", "famfamfam-flags gb"),
-                new ApplicationLanguage(tenantId, "ar", "العربية", "famfamfam-flags sa"),
-                new ApplicationLanguage(tenantId, "de", "German", "famfamfam-flags de"),
-                new ApplicationLanguage(tenantId, "it", "Italiano", "famfamfam-flags it"),
-                new ApplicationLanguage(tenantId, "fr", "Français", "famfamfam-flags fr"),
-                new ApplicationLanguage(tenantId, "pt-BR", "Português", "famfamfam-flags br"),
-                new ApplicationLanguage(tenantId, "tr", "Türkçe", "famfamfam-flags tr"),
-                new ApplicationLanguage(tenantId, "ru", "Русский", "famfamfam-flags ru"),
                 new ApplicationLanguage(tenantId, "zh-Hans", "简体中文", "famfamfam-flags cn"),
-                new ApplicationLanguage(tenantId, "es-MX", "Español México", "famfamfam-flags mx"),
-                new ApplicationLanguage(tenantId, "nl", "Nederlands", "famfamfam-flags nl"),
-                new ApplicationLanguage(tenantId, "ja", "日本語", "famfamfam-flags jp"),
             };
         }
 
