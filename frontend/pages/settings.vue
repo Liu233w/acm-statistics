05ace16c5cd7a2a07a265c9bc4dede096a9f2ed4
Liu233w <Liu233w@users.noreply.github.com>
2020-04-02 20:32:56 +1100

    feat(frontend): 调通删除用户的功能
    resolve #917

--- frontend/pages/settings.vue
+++ frontend/pages/settings.vue
@@ -35,7 +35,7 @@
             <v-list-item
               input-value="true"
               color="red"
-              @click=";"
+              @click="deleteDialog = true"
             >
               <v-list-item-content>
                 <v-list-item-title>删除帐号</v-list-item-title>
@@ -46,15 +46,56 @@
         </v-card>
       </v-flex>
     </v-layout>
+    <v-dialog
+      v-model="deleteDialog"
+      persistent
+      max-width="400"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          您确定要删除本帐号吗？
+        </v-card-title>
+        <v-card-text>
+          删除之后，您将不再收到题量统计邮件。之前统计的数据都会丢失。
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="darken-1"
+            text
+            @click="deleteDialog = false"
+          >
+            取消
+          </v-btn>
+          <v-btn
+            color="red darken-1"
+            text
+            @click="deleteAccount"
+          >
+            删除
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      deleteDialog: false,
+    }
+  },
   methods: {
     async logout() {
       this.$store.dispatch('session/logout')
       await this.$store.dispatch('session/refreshUser')
+      this.$router.push('/')
+    },
+    async deleteAccount() {
+      await this.$axios.$post('/api/services/app/Account/SelfDelete')
+      this.$store.dispatch('session/logout')
       this.$router.push('/')
     },
   },
