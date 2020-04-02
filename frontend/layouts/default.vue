98dee6410417714120cd9467c18d138eeaa09025
Liu233w <Liu233w@users.noreply.github.com>
2020-04-02 13:02:17 +1100

    feat(frontend): 添加用户控制面板
    * 登出功能移动到控制面板
    + 添加删除帐号按钮（没有实际功能）
    
    related to #917

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <VApp>
-    <VNavigationDrawer v-model="drawer" clipped fixed app>
+    <VNavigationDrawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+    >
       <VList>
-        <VListItem nuxt :to="item.to" :key="i" v-for="(item, i) in items" exact>
+        <VListItem
+          nuxt
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
           <VListItemAction>
             <VIcon v-html="item.icon" />
           </VListItemAction>
@@ -12,7 +23,11 @@
         </VListItem>
       </VList>
     </VNavigationDrawer>
-    <VAppBar fixed app clipped-left>
+    <VAppBar
+      fixed
+      app
+      clipped-left
+    >
       <VAppBarNavIcon @click="drawer = !drawer" />
       <VToolbarTitle v-text="title" />
       <VSpacer />
@@ -37,7 +52,10 @@
     <VContent>
       <Nuxt />
     </VContent>
-    <VFooter fixed app>
+    <VFooter
+      fixed
+      app
+    >
       <span class="body-2">&copy; 2018 - {{ buildYear }} NWPU-ACM 技术组</span>
       <VSpacer />
       <span class="body-2">陕ICP备17008184号</span>
@@ -46,6 +64,7 @@
 </template>
 
 <script>
+import _ from 'lodash'
 import { getDateFromTimestamp } from '~/components/utils'
 import UserStatus from '~/components/UserStatus'
 
@@ -56,14 +75,34 @@ export default {
   data() {
     return {
       drawer: true,
-      items: [
-        { icon: 'home', title: '返回主页', to: '/' },
-        { icon: 'code', title: 'OJ题量统计', to: '/statistics' },
-        { icon: 'info', title: '关于', to: '/about' },
-      ],
       title: 'NWPU-ACM 查询系统',
       buildYear: getDateFromTimestamp(this.$env.BUILD_TIME).getFullYear(),
     }
   },
+  computed: {
+    items() {
+      return _.filter(
+        [
+          { icon: 'home', title: '返回主页', to: '/' },
+          { icon: 'code', title: 'OJ题量统计', to: '/statistics' },
+          ifTrue(this.$store.state.session.login, {
+            icon: 'settings',
+            title: '用户设置',
+            to: '/settings',
+          }),
+          { icon: 'info', title: '关于', to: '/about' },
+        ],
+        item => item !== null,
+      )
+    },
+  },
+}
+
+function ifTrue(condition, item) {
+  if (condition) {
+    return item
+  } else {
+    return null
+  }
 }
 </script>
---
+++ frontend/pages/settings.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex
+        xs12
+        sm6
+        offset-sm3
+      >
+        <v-card>
+          <v-app-bar
+            color="light-blue"
+            dark
+          >
+            <v-toolbar-title>用户设置</v-toolbar-title>
+            <v-spacer />
+          </v-app-bar>
+          <v-list
+            subheader
+            three-line
+          >
+            <v-subheader>帐号动作</v-subheader>
+            <v-list-item @click="logout">
+              <v-list-item-content>
+                <v-list-item-title>注销（登出）</v-list-item-title>
+                <v-list-item-subtitle>退出本帐号</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <v-divider />
+          <v-list
+            subheader
+            three-line
+          >
+            <v-subheader>帐号设置</v-subheader>
+            <v-list-item
+              input-value="true"
+              color="red"
+              @click=";"
+            >
+              <v-list-item-content>
+                <v-list-item-title>删除帐号</v-list-item-title>
+                <v-list-item-subtitle>删除本帐号和与其关联的邮箱</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    async logout() {
+      this.$store.dispatch('session/logout')
+      await this.$store.dispatch('session/refreshUser')
+      this.$router.push('/')
+    },
+  },
+}
+</script>
