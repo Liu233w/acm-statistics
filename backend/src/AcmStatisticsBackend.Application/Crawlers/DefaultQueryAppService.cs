d3aa8fec0ac9806b874c10a11a149c4808725074
Liu233w <Liu233w@users.noreply.github.com>
2020-04-10 01:53:13 +1000

    feat(backend): 完成保存用户名的基础结构
    还没填充内容

---
+++ backend/src/AcmStatisticsBackend.Application/Crawlers/DefaultQueryAppService.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Authorization;
+using AcmStatisticsBackend.Authorization;
+using AcmStatisticsBackend.Crawlers.Dto;
+
+namespace AcmStatisticsBackend.Crawlers
+{
+    /// <inheritdoc cref="IDefaultQueryAppService"/>
+    [AbpAuthorize(PermissionNames.Statistics_DefaultQuery)]
+    public class DefaultQueryAppService : AcmStatisticsBackendAppServiceBase, IDefaultQueryAppService
+    {
+        /// <inheritdoc cref="IDefaultQueryAppService.GetDefaultQueries"/>
+        public DefaultQueryDto GetDefaultQueries()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <inheritdoc cref="IDefaultQueryAppService.SetDefaultQueries"/>
+        public void SetDefaultQueries(DefaultQueryDto dto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
