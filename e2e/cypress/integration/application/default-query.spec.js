efd73a50471e113b58b12060ede8850d95b328c0
Liu233w <Liu233w@users.noreply.github.com>
2020-04-10 21:31:50 +1000

    test(e2e): 修复一个default query 相关的bug

--- e2e/cypress/integration/application/default-query.spec.js
+++ e2e/cypress/integration/application/default-query.spec.js
@@ -3,19 +3,24 @@ Cypress.config('baseUrl', 'http://localhost:3000')
 describe('未登录时将用户常用查询保存到Cookie', () => {
 
   it('能够正常运作', () => {
+    // arrange
     cy.visit('/statistics')
 
     cy.get('.v-input:contains("统一设置用户名") input').should('have.value', '')
     cy.get('.worker-item:contains("CodeForces") input').should('have.value', '')
 
-    cy.get('.worker-item:contains("HDU") input').type('user1')
+    cy.get('.v-input:contains("统一设置用户名") input').type('main-name')
+    // 等待其他worker更新
+    cy.get('.worker-item:contains("CodeForces") input').should('have.value', 'main-name')
+    cy.get('.worker-item:contains("HDU") input').clear().type('user1')
 
+    // act
     cy.contains('保存用户名').click()
     cy.contains('保存成功')
 
     cy.reload()
     cy.get('.worker-item:contains("HDU") input').should('have.value', 'user1')
-    cy.get('.v-input:contains("统一设置用户名") input').should('have.value', '')
+    cy.get('.v-input:contains("统一设置用户名") input').should('have.value', 'main-name')
 
     cy.matchImageSnapshot()
   })
@@ -39,7 +44,10 @@ describe('登录时将用户常用查询保存到服务器', () => {
     cy.get('.v-input:contains("统一设置用户名") input').should('have.value', '')
     cy.get('.worker-item:contains("CodeForces") input').should('have.value', '')
 
-    cy.get('.worker-item:contains("HDU") input').type('user1')
+    cy.get('.v-input:contains("统一设置用户名") input').type('main-name')
+    // 等待其他worker更新
+    cy.get('.worker-item:contains("CodeForces") input').should('have.value', 'main-name')
+    cy.get('.worker-item:contains("HDU") input').clear().type('user1')
 
     cy.contains('保存用户名').click()
     cy.contains('保存成功')
@@ -55,7 +63,9 @@ describe('登录时将用户常用查询保存到服务器', () => {
 
     cy.visit('/statistics')
     cy.get('.worker-item:contains("HDU") input').should('have.value', 'user1')
-    cy.get('.v-input:contains("统一设置用户名") input').should('have.value', '')
+    cy.get('.v-input:contains("统一设置用户名") input').should('have.value', 'main-name')
+
+    cy.get('.worker-item:contains("CodeForces") input').should('have.value', 'main-name')
 
     // 隐藏用户名
     cy.get(`button:contains("${username}")`)
