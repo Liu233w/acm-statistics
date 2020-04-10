c5f08027bced8c49126beda4b4d492740f5aaff0
Liu233w <Liu233w@users.noreply.github.com>
2020-04-10 19:39:28 +1000

    feat(backend): 添加数据库迁移

---
+++ backend/src/AcmStatisticsBackend.EntityFrameworkCore/Migrations/20200410093107_AddDefaultQuery.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcmStatisticsBackend.Migrations
+{
+    public partial class AddDefaultQuery : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AbpOrganizationUnits_TenantId_Code",
+                table: "AbpOrganizationUnits");
+
+            migrationBuilder.CreateTable(
+                name: "AbpDynamicParameters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ParameterName = table.Column<string>(nullable: true),
+                    InputType = table.Column<string>(nullable: true),
+                    Permission = table.Column<string>(nullable: true),
+                    TenantId = table.Column<int>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpDynamicParameters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DefaultQueries",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    LastModifierUserId = table.Column<long>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeleterUserId = table.Column<long>(nullable: true),
+                    DeletionTime = table.Column<DateTime>(nullable: true),
+                    UserId = table.Column<long>(nullable: false),
+                    MainUsername = table.Column<string>(nullable: false),
+                    UsernamesInCrawlers = table.Column<string>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DefaultQueries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DefaultQueries_AbpUsers_CreatorUserId",
+                        column: x => x.CreatorUserId,
+                        principalTable: "AbpUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_DefaultQueries_AbpUsers_DeleterUserId",
+                        column: x => x.DeleterUserId,
+                        principalTable: "AbpUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_DefaultQueries_AbpUsers_LastModifierUserId",
+                        column: x => x.LastModifierUserId,
+                        principalTable: "AbpUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_DefaultQueries_AbpUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AbpUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpDynamicParameterValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Value = table.Column<string>(nullable: false),
+                    TenantId = table.Column<int>(nullable: true),
+                    DynamicParameterId = table.Column<int>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpDynamicParameterValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpDynamicParameterValues_AbpDynamicParameters_DynamicParame~",
+                        column: x => x.DynamicParameterId,
+                        principalTable: "AbpDynamicParameters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpEntityDynamicParameters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EntityFullName = table.Column<string>(nullable: true),
+                    DynamicParameterId = table.Column<int>(nullable: false),
+                    TenantId = table.Column<int>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpEntityDynamicParameters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpEntityDynamicParameters_AbpDynamicParameters_DynamicParam~",
+                        column: x => x.DynamicParameterId,
+                        principalTable: "AbpDynamicParameters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpEntityDynamicParameterValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Value = table.Column<string>(nullable: false),
+                    EntityId = table.Column<string>(nullable: true),
+                    EntityDynamicParameterId = table.Column<int>(nullable: false),
+                    TenantId = table.Column<int>(nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpEntityDynamicParameterValues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AbpEntityDynamicParameterValues_AbpEntityDynamicParameters_E~",
+                        column: x => x.EntityDynamicParameterId,
+                        principalTable: "AbpEntityDynamicParameters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpOrganizationUnits_TenantId_Code",
+                table: "AbpOrganizationUnits",
+                columns: new[] { "TenantId", "Code" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpDynamicParameters_ParameterName_TenantId",
+                table: "AbpDynamicParameters",
+                columns: new[] { "ParameterName", "TenantId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpDynamicParameterValues_DynamicParameterId",
+                table: "AbpDynamicParameterValues",
+                column: "DynamicParameterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpEntityDynamicParameters_DynamicParameterId",
+                table: "AbpEntityDynamicParameters",
+                column: "DynamicParameterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpEntityDynamicParameters_EntityFullName_DynamicParameterId~",
+                table: "AbpEntityDynamicParameters",
+                columns: new[] { "EntityFullName", "DynamicParameterId", "TenantId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpEntityDynamicParameterValues_EntityDynamicParameterId",
+                table: "AbpEntityDynamicParameterValues",
+                column: "EntityDynamicParameterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DefaultQueries_CreatorUserId",
+                table: "DefaultQueries",
+                column: "CreatorUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DefaultQueries_DeleterUserId",
+                table: "DefaultQueries",
+                column: "DeleterUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DefaultQueries_LastModifierUserId",
+                table: "DefaultQueries",
+                column: "LastModifierUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DefaultQueries_UserId",
+                table: "DefaultQueries",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AbpDynamicParameterValues");
+
+            migrationBuilder.DropTable(
+                name: "AbpEntityDynamicParameterValues");
+
+            migrationBuilder.DropTable(
+                name: "DefaultQueries");
+
+            migrationBuilder.DropTable(
+                name: "AbpEntityDynamicParameters");
+
+            migrationBuilder.DropTable(
+                name: "AbpDynamicParameters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AbpOrganizationUnits_TenantId_Code",
+                table: "AbpOrganizationUnits");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpOrganizationUnits_TenantId_Code",
+                table: "AbpOrganizationUnits",
+                columns: new[] { "TenantId", "Code" });
+        }
+    }
+}
