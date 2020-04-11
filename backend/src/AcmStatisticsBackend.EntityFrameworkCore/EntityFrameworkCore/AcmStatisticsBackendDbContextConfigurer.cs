a9a47a77acfb1539c1e110cae2ffeb9bcf37cd9e
Liu233w <Liu233w@users.noreply.github.com>
2020-04-11 22:47:02 +1000

    feat(backend): 添加entity framework lazy-loading

--- backend/src/AcmStatisticsBackend.EntityFrameworkCore/EntityFrameworkCore/AcmStatisticsBackendDbContextConfigurer.cs
+++ backend/src/AcmStatisticsBackend.EntityFrameworkCore/EntityFrameworkCore/AcmStatisticsBackendDbContextConfigurer.cs
@@ -5,14 +5,20 @@ namespace AcmStatisticsBackend.EntityFrameworkCore
 {
     public static class AcmStatisticsBackendDbContextConfigurer
     {
-        public static void Configure(DbContextOptionsBuilder<AcmStatisticsBackendDbContext> builder, string connectionString)
+        public static void Configure(DbContextOptionsBuilder<AcmStatisticsBackendDbContext> builder,
+            string connectionString)
         {
-            builder.UseMySql(connectionString);
+            builder
+                .UseLazyLoadingProxies()
+                .UseMySql(connectionString);
         }
 
-        public static void Configure(DbContextOptionsBuilder<AcmStatisticsBackendDbContext> builder, DbConnection connection)
+        public static void Configure(DbContextOptionsBuilder<AcmStatisticsBackendDbContext> builder,
+            DbConnection connection)
         {
-            builder.UseMySql(connection);
+            builder
+                .UseLazyLoadingProxies()
+                .UseMySql(connection);
         }
     }
 }
