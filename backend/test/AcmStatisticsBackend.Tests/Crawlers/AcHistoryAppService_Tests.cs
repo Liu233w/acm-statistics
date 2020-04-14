eb9f39774eb76604609f1ff54d29db618f670884
Liu233w <Liu233w@users.noreply.github.com>
2020-04-15 01:25:43 +1000

    fix(backend): 完成AcHistory测试，修复bug

--- backend/test/AcmStatisticsBackend.Tests/Crawlers/AcHistoryAppService_Tests.cs
+++ backend/test/AcmStatisticsBackend.Tests/Crawlers/AcHistoryAppService_Tests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
 using Abp.Timing;
 using AcmStatisticsBackend.Crawlers;
 using AcmStatisticsBackend.Crawlers.Dto;
@@ -252,6 +253,170 @@ namespace AcmStatisticsBackend.Tests.Crawlers
             {
                 (await ctx.AcHistories.CountAsync()).ShouldBe(0);
                 (await ctx.AcWorkerHistories.CountAsync()).ShouldBe(0);
+            });
+        }
+
+        [Fact]
+        public async Task GetAcHistory_能够正确获取数据()
+        {
+            // arrange
+            LoginAsDefaultTenantAdmin();
+            await UsingDbContextAsync(async ctx =>
+            {
+                Debug.Assert(AbpSession.UserId != null, "AbpSession.UserId != null");
+                await ctx.AcHistories.AddAsync(new AcHistory
+                {
+                    Solved = 1,
+                    Submission = 10,
+                    MainUsername = "u1",
+                    CreationTime = new DateTime(2020, 4, 1, 10, 0, 0),
+                    UserId = AbpSession.UserId.Value,
+                });
+            });
+
+            // act
+            var result = await _acHistoryAppService.GetAcHistory(new PagedResultRequestDto
+            {
+                SkipCount = 0,
+                MaxResultCount = 5,
+            });
+
+            // assert
+            result.TotalCount.ShouldBe(1);
+            result.Items[0].WithIn(it =>
+            {
+                it.Solved.ShouldBe(1);
+                it.Submission.ShouldBe(10);
+                it.MainUsername.ShouldBe("u1");
+                it.CreationTime.ShouldBe(new DateTime(2020, 4, 1, 10, 0, 0));
+            });
+        }
+
+        [Fact]
+        public async Task GetAcHistory_能够按照正确顺序获取数据()
+        {
+            // arrange
+            LoginAsDefaultTenantAdmin();
+            await UsingDbContextAsync(async ctx =>
+            {
+                Debug.Assert(AbpSession.UserId != null, "AbpSession.UserId != null");
+                await ctx.AcHistories.AddRangeAsync(new[]
+                {
+                    new AcHistory
+                    {
+                        Solved = 1,
+                        Submission = 10,
+                        MainUsername = "u1",
+                        CreationTime = new DateTime(2020, 4, 1, 10, 0, 0),
+                        UserId = AbpSession.UserId.Value,
+                    },
+                    new AcHistory
+                    {
+                        Solved = 2,
+                        Submission = 11,
+                        MainUsername = "u1",
+                        CreationTime = new DateTime(2020, 4, 2, 10, 0, 0),
+                        UserId = AbpSession.UserId.Value,
+                    },
+                });
+            });
+
+            // act
+            var result = await _acHistoryAppService.GetAcHistory(new PagedResultRequestDto
+            {
+                SkipCount = 0,
+                MaxResultCount = 5,
+            });
+
+            // assert
+            result.TotalCount.ShouldBe(2);
+            result.Items[0].WithIn(it =>
+            {
+                it.Solved.ShouldBe(2);
+                it.Submission.ShouldBe(11);
+                it.MainUsername.ShouldBe("u1");
+                it.CreationTime.ShouldBe(new DateTime(2020, 4, 2, 10, 0, 0));
+            });
+            result.Items[1].WithIn(it =>
+            {
+                it.Solved.ShouldBe(1);
+                it.Submission.ShouldBe(10);
+                it.MainUsername.ShouldBe("u1");
+                it.CreationTime.ShouldBe(new DateTime(2020, 4, 1, 10, 0, 0));
+            });
+        }
+
+        [Fact]
+        public async Task GetAcWorkerHistory_能够获取正确记录()
+        {
+            // arrange
+            LoginAsDefaultTenantAdmin();
+            _testClockProvider.Now = new DateTime(2020, 4, 1, 10, 0, 0);
+            await _acHistoryAppService.SaveOrReplaceAcHistory(new SaveOrReplaceAcHistoryInput
+            {
+                Solved = 3,
+                Submission = 20,
+                MainUsername = "mainUser",
+                AcWorkerHistories = new List<AcWorkerHistoryDto>
+                {
+                    new AcWorkerHistoryDto
+                    {
+                        Username = "u1",
+                        CrawlerName = "c1",
+                        Solved = 3,
+                        Submission = 20,
+                        ErrorMessage = null,
+                        HasSolvedList = true,
+                        SolvedList = new string[]
+                        {
+                            "p1",
+                            "p2",
+                            "p3",
+                        },
+                    },
+                    new AcWorkerHistoryDto
+                    {
+                        Username = "u2",
+                        CrawlerName = "c2",
+                        ErrorMessage = "Cannot find username",
+                    },
+                },
+            });
+
+            var historyResponse = await _acHistoryAppService.GetAcHistory(new PagedResultRequestDto());
+            var historyId = historyResponse.Items[0].Id;
+
+            // act
+            var list = await _acHistoryAppService.GetAcWorkerHistory(new GetAcWorkerHistoryInput
+            {
+                AcHistoryId = historyId,
+            });
+
+            // assert
+            list.Items.Count.ShouldBe(2);
+            list.Items[0].WithIn(it =>
+            {
+                it.Solved.ShouldBe(3);
+                it.Submission.ShouldBe(20);
+                it.Username.ShouldBe("u1");
+                it.CrawlerName.ShouldBe("c1");
+                it.ErrorMessage.ShouldBeNull();
+                it.HasSolvedList.ShouldBe(true);
+                it.SolvedList.ShouldEqualInJson(new string[]
+                {
+                    "p1",
+                    "p2",
+                    "p3",
+                });
+            });
+            list.Items[1].WithIn(it =>
+            {
+                it.Solved.ShouldBe(0);
+                it.Username.ShouldBe("u2");
+                it.CrawlerName.ShouldBe("c2");
+                it.ErrorMessage.ShouldBe("Cannot find username");
+                it.HasSolvedList.ShouldBe(false);
+                it.SolvedList.ShouldEqualInJson(new string[0]);
             });
         }
     }
