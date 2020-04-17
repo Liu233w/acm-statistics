43bd3f09e0b6ffb5e2143a7fda99762441350ecd
Liu233w <Liu233w@users.noreply.github.com>
2020-04-18 02:28:45 +1000

    feat(frontend): change settings appearance and add changing password
    related to #983

--- frontend/pages/settings.vue
+++ frontend/pages/settings.vue
@@ -1,45 +1,102 @@
 <template>
-  <v-container>
-    <v-layout>
-      <v-flex
-        xs12
-        sm6
-        offset-sm3
-      >
+  <v-container fluid>
+    <v-row>
+      <v-col>
         <v-card>
-          <v-app-bar
-            color="light-blue"
-            dark
-          >
-            <v-toolbar-title>Settings</v-toolbar-title>
-            <v-spacer />
-          </v-app-bar>
-          <v-list
-            subheader
-            three-line
-          >
-            <v-subheader>Accounts</v-subheader>
-            <v-list-item @click="logout">
-              <v-list-item-content>
-                <v-list-item-title>Sign out</v-list-item-title>
-                <v-list-item-subtitle>Logout of this computer</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-            <v-divider />
-            <v-list-item
-              input-value="true"
-              color="red"
-              @click="deleteDialog = true"
+          <v-card-title>Sign out</v-card-title>
+          <v-card-text>
+            Logout of this computer.
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              @click="logout"
+              text
             >
-              <v-list-item-content>
-                <v-list-item-title>Delete Account</v-list-item-title>
-                <v-list-item-subtitle>Delete this account and all data related to it</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
+              sign out
+            </v-btn>
+          </v-card-actions>
         </v-card>
-      </v-flex>
-    </v-layout>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>Change Password</v-card-title>
+          <v-card-text>
+            <v-form v-model="valid">
+              <v-text-field
+                prepend-icon="lock"
+                label="Current Password"
+                type="password"
+                v-model="currentPwd"
+                required
+                :rules="[rules.required]"
+              />
+              <v-text-field
+                prepend-icon="lock"
+                label="New Password"
+                type="password"
+                v-model="newPwd"
+                required
+                :rules="[rules.required, rules.password]"
+              />
+              <v-text-field
+                prepend-icon="lock"
+                label="Confirm password"
+                type="password"
+                v-model="pwdRepeat"
+                required
+                :rules="[rules.required, () => newPwd === pwdRepeat || 'Password must match']"
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              color="info"
+              text
+              :disabled="!valid"
+              @click="changePassword"
+              :loading="loading"
+            >
+              submit
+            </v-btn>
+          </v-card-actions>
+          <v-overlay
+            :absolute="true"
+            :value="changePwdMessage"
+          >
+            <v-chip
+              :color="changePwdMessage.color"
+              v-if="changePwdMessage"
+              @click="changePwdMessage = null"
+            >
+              {{ changePwdMessage.message }}
+            </v-chip>
+          </v-overlay>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title class="red--text">
+            Delete Account
+          </v-card-title>
+          <v-card-text>
+            Delete this account and all data related to it.
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              @click="deleteDialog = true"
+              text
+              color="red"
+            >
+              delete
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
     <v-dialog
       v-model="deleteDialog"
       persistent
@@ -75,10 +132,20 @@
 </template>
 
 <script>
+import rulesMixin from '~/components/rulesMixin'
+import { getAbpErrorMessage } from '~/components/utils'
+
 export default {
+  mixins: [rulesMixin],
   data() {
     return {
       deleteDialog: false,
+      currentPwd: '',
+      newPwd: '',
+      pwdRepeat: '',
+      valid: false,
+      loading: false,
+      changePwdMessage: null,
     }
   },
   methods: {
@@ -91,6 +158,24 @@ export default {
       await this.$axios.$post('/api/services/app/Account/SelfDelete')
       this.$store.dispatch('session/logout')
       this.$router.push('/')
+    },
+    async changePassword() {
+      try {
+        await this.$axios.$post('/api/services/app/Account/ChangePassword', {
+          currentPassword: this.currentPwd,
+          newPassword: this.newPwd,
+        })
+        this.changePwdMessage = {
+          color: 'success',
+          message: 'Success!',
+        }
+      } catch (err) {
+
+        this.changePwdMessage = {
+          color: 'error',
+          message: getAbpErrorMessage(err),
+        }
+      }
     },
   },
 }
