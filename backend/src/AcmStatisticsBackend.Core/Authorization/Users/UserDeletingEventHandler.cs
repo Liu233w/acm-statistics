a6d4c0333d8064b8144fdc801f4bfc88aea4fa02
Liu233w <Liu233w@users.noreply.github.com>
2020-04-18 16:07:12 +1000

    refactor(backend): use domain event to delete other entities

---
+++ backend/src/AcmStatisticsBackend.Core/Authorization/Users/UserDeletingEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Dynamic.Core;
+using System.Threading.Tasks;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
+using Abp.Events.Bus.Entities;
+using Abp.Events.Bus.Handlers;
+using AcmStatisticsBackend.Crawlers;
+
+namespace AcmStatisticsBackend.Authorization.Users
+{
+    public class UserDeletingEventHandler : IAsyncEventHandler<EntityDeletingEventData<User>>, ITransientDependency
+    {
+        private readonly IRepository<DefaultQuery, long> _defaultQueryRepository;
+        private readonly IRepository<AcHistory, long> _acHistoryRepository;
+
+        public UserDeletingEventHandler(IRepository<DefaultQuery, long> defaultQueryRepository,
+            IRepository<AcHistory, long> acHistoryRepository)
+        {
+            _defaultQueryRepository = defaultQueryRepository;
+            _acHistoryRepository = acHistoryRepository;
+        }
+
+        public async Task HandleEventAsync(EntityDeletingEventData<User> eventData)
+        {
+            await _defaultQueryRepository.HardDeleteAsync(
+                e => e.UserId == eventData.Entity.Id);
+            await _acHistoryRepository.DeleteAsync(
+                e => e.UserId == eventData.Entity.Id);
+        }
+    }
+}
