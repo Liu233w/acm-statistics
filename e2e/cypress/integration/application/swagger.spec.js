2ad91ef290c440353a74f3e2f1c4f343a53dc4ab
Liu233w <Liu233w@users.noreply.github.com>
2020-05-02 22:55:42 +1000

    test(e2e): skip one test
    Currently admin is not needed, so the test should be disabled

--- e2e/cypress/integration/application/swagger.spec.js
+++ e2e/cypress/integration/application/swagger.spec.js
@@ -4,19 +4,19 @@ beforeEach(() => {
   cy.visit('/swagger')
 })
 
-it('能够加载crawler-api-backend的swagger文件', () => {
+it('can load swagger file of crawler-api-backend', () => {
   cy.get('#select').select('爬虫 API', {timeout: 10000})
   cy.contains('crawler-api-backend', {timeout: 10000})
 })
 
-describe('能够加载后端的swagger文件', () => {
+describe('can load swagger file of backend', () => {
 
-  it('能够访问页面', () => {
+  it('can visit page', () => {
     cy.get('#select').select('后端 API V1', {timeout: 10000})
     cy.contains('AcmStatisticsBackend API', {timeout: 60000})
   })
 
-  it('可以正常登录', () => {
+  it.skip('can login correctly', () => {
     cy.get('#select').select('后端 API V1', {timeout: 10000})
     cy.get('#authorize', {timeout: 10000}).within(()=>{
       cy.contains('Authorize').click()
