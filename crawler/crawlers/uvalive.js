4c493e8ffe73a6c123792e6fc7a33dee5490cecd
Liu233w <Liu233w@users.noreply.github.com>
2020-05-19 19:41:18 +1000

    fix(crawler): fix problem id in uvalive

--- crawler/crawlers/uvalive.js
+++ crawler/crawlers/uvalive.js
@@ -1,5 +1,7 @@
 const request = require('superagent')
 
+const prefix = 'https://icpcarchive.ecs.baylor.edu/uhunt'
+
 /**
  * It has the same interface as uva.
  * 
@@ -11,14 +13,14 @@ module.exports = async function (config, username) {
     throw new Error('Please enter username')
   }
 
-  const uidRes = await request.get('https://icpcarchive.ecs.baylor.edu/uhunt/api/uname2uid/' + username)
+  const uidRes = await request.get(prefix + '/api/uname2uid/' + username)
 
   if (uidRes.body === 0) {
     throw new Error('The user does not exist')
   }
 
   const res = await request
-    .get('https://icpcarchive.ecs.baylor.edu/uhunt/api/subs-user/' + uidRes.body)
+    .get(prefix + '/api/subs-user/' + uidRes.body)
 
   const acSet = new Set()
   const problemArray = res.body.subs
@@ -28,14 +30,20 @@ module.exports = async function (config, username) {
     }
   })
 
-  const solvedList = []
+  const solvedListPromises = []
   for (let item of acSet) {
-    solvedList.push(String(item))
+    solvedListPromises.push(getProblemNumFromProblemId(item))
   }
 
   return {
     solved: acSet.size,
     submissions: problemArray.length,
-    solvedList,
+    solvedList: await Promise.all(solvedListPromises),
   }
+}
+
+async function getProblemNumFromProblemId(pid) {
+  // in uva, problem id (pid) is different from problem number (num)
+  const res = await request.get(prefix + '/api/p/id/' + pid)
+  return String(res.body.num)
 }
