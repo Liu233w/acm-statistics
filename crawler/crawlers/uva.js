e31eb9e5221d18b72559693390dede75e6cc985c
Liu233w <Liu233w@users.noreply.github.com>
2020-05-19 19:51:24 +1000

    fix(crawler): fix uva problem number
    use the code in uvalive

--- crawler/crawlers/uva.js
+++ crawler/crawlers/uva.js
@@ -1,5 +1,7 @@
 const request = require('superagent')
 
+const prefix = 'https://uhunt.onlinejudge.org'
+
 /** uva 的 api 接口：https://uhunt.onlinejudge.org/api
  * 由于uva不支持分页处理，每次必须把全部信息拉取下来，在前端查询时有可能超过 cors-proxy 的长度限制
  * 不过普通用户的话也不会有太多数据
@@ -10,14 +12,14 @@ module.exports = async function (config, username) {
     throw new Error('Please enter username')
   }
 
-  const uidRes = await request.get('https://uhunt.onlinejudge.org/api/uname2uid/' + username)
+  const uidRes = await request.get(prefix + '/api/uname2uid/' + username)
 
   if (uidRes.body === 0) {
     throw new Error('The user does not exist')
   }
 
   const res = await request
-    .get('https://uhunt.onlinejudge.org/api/subs-user/' + uidRes.body)
+    .get(prefix + '/api/subs-user/' + uidRes.body)
 
   const acSet = new Set()
   const problemArray = res.body.subs
@@ -27,14 +29,20 @@ module.exports = async function (config, username) {
     }
   })
 
-  const solvedList = []
+  const solvedListPromises = []
   for (let item of acSet) {
-    solvedList.push(String(item))
+    solvedListPromises.push(getProblemNumFromProblemId(item))
   }
 
   return {
     solved: acSet.size,
     submissions: problemArray.length,
-    solvedList,
+    solvedList: await Promise.all(solvedListPromises),
   }
+}
+
+async function getProblemNumFromProblemId(pid) {
+  // in uva, problem id (pid) is different from problem number (num)
+  const res = await request.get(prefix + '/api/p/id/' + pid)
+  return String(res.body.num)
 }
