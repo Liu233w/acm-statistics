7a430657bafd930796248fdb04093fa951e53788
Liu233w <Liu233w@users.noreply.github.com>
2020-05-20 21:39:51 +1000

    test(backend): move TestClockProvider out

---
+++ backend/test/AcmStatisticsBackend.Tests/TestClockProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Abp.Timing;
+
+namespace AcmStatisticsBackend.Tests
+{
+    internal class TestClockProvider : IClockProvider
+    {
+        public DateTime Normalize(DateTime dateTime)
+        {
+            return ClockProviders.Utc.Normalize(dateTime);
+        }
+
+        public DateTime Now { get; set; }
+
+        public DateTimeKind Kind => DateTimeKind.Utc;
+
+        public bool SupportsMultipleTimezone => true;
+    }
+}
