9ed10a01afccfc4fc51d30b5b6e0a82328d981cb
Liu233w <Liu233w@users.noreply.github.com>
2020-05-20 21:40:22 +1000

    feat(backend): add UserTimeZoneManager

---
+++ backend/src/AcmStatisticsBackend.Core/Timing/UserTimeZoneManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Abp;
+using Abp.Configuration;
+using Abp.Dependency;
+using Abp.Timing;
+
+namespace AcmStatisticsBackend.Timing
+{
+    public class UserTimeZoneManager : ITransientDependency
+    {
+        private readonly ISettingManager _settingManager;
+
+        public UserTimeZoneManager(ISettingManager settingManager)
+        {
+            _settingManager = settingManager;
+        }
+
+        /// <summary>
+        /// Get the time zone id of given user.
+        /// </summary>
+        /// <returns>Time zone of the user. It is a windows time zone name.</returns>
+        public Task<string> GetTimeZoneNameOfUserAsync(UserIdentifier userIdentifier)
+        {
+            return _settingManager.GetSettingValueForUserAsync(
+                TimingSettingNames.TimeZone, userIdentifier);
+        }
+
+        /// <summary>
+        /// Get the TimeZoneInfo of given user.
+        /// </summary>
+        public async Task<TimeZoneInfo> GetTimeZoneOfUserAsync(UserIdentifier userIdentifier)
+        {
+            return TimeZoneInfo.FindSystemTimeZoneById(await GetTimeZoneNameOfUserAsync(userIdentifier));
+        }
+
+        /// <summary>
+        /// Set the time zone of given user.
+        /// </summary>
+        /// <param name="userIdentifier">The user identifier</param>
+        /// <param name="timeZoneName">Time zone of the user. It is a windows time zone name.</param>
+        public Task SetTimeZoneOfUserAsync(UserIdentifier userIdentifier, string timeZoneName)
+        {
+            return _settingManager.ChangeSettingForUserAsync(
+                userIdentifier,
+                TimingSettingNames.TimeZone,
+                timeZoneName);
+        }
+    }
+}
