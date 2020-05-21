138408ffba38d0ac3195299ee8a7dffc7abfe9f5
Liu233w <Liu233w@users.noreply.github.com>
2020-05-21 14:33:00 +1000

    docs(backend): update some chinese docs to english

--- backend/src/AcmStatisticsBackend.Application/Crawlers/Dto/DefaultQueryDto.cs
+++ backend/src/AcmStatisticsBackend.Application/Crawlers/Dto/DefaultQueryDto.cs
@@ -6,22 +6,23 @@ using Abp.Runtime.Validation;
 namespace AcmStatisticsBackend.Crawlers.Dto
 {
     /// <summary>
-    /// 用户的默认爬虫用户名
+    /// Store default query usernames
     /// </summary>
     [AutoMap(typeof(DefaultQuery))]
     public class DefaultQueryDto : ICustomValidate
     {
         /// <summary>
-        /// 主用户名
+        /// main username
         /// </summary>
-        /// [Required]
         [MinLength(0)]
         public string MainUsername { get; set; } = "";
 
         /// <summary>
-        /// 在各个爬虫上的用户名。key为爬虫名称，value为一个用户名的列表，表示在该爬虫上的所有用户名。
+        /// Usernames in each crawlers. Key is the name of crawler, value is a list that contains
+        /// all usernames in this crawler.
         /// </summary>
-        public Dictionary<string, List<string>> UsernamesInCrawlers { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> UsernamesInCrawlers { get; set; } =
+            new Dictionary<string, List<string>>();
 
         public void AddValidationErrors(CustomValidationContext context)
         {
--- backend/src/AcmStatisticsBackend.Application/Crawlers/Dto/SaveOrReplaceQueryHistoryInput.cs
+++ backend/src/AcmStatisticsBackend.Application/Crawlers/Dto/SaveOrReplaceQueryHistoryInput.cs
@@ -8,20 +8,28 @@ namespace AcmStatisticsBackend.Crawlers.Dto
     [AutoMapTo(typeof(QueryHistory))]
     public class SaveOrReplaceQueryHistoryInput
     {
-        /// <inheritdoc cref="QueryHistory.MainUsername"/>
+        /// <summary>
+        /// Main username of query history, can be empty
+        /// </summary>
         [Required]
         [MinLength(0)]
         public string MainUsername { get; set; }
 
-        /// <inheritdoc cref="QueryHistory.Submission"/>
+        /// <summary>
+        /// Total submission count
+        /// </summary>
         [Range(0, int.MaxValue)]
         public int Submission { get; set; }
 
-        /// <inheritdoc cref="QueryHistory.Solved"/>
+        /// <summary>
+        /// Total solved count, redundant problems (including problems in virtual_judge) are removed.
+        /// </summary>
         [Range(0, int.MaxValue)]
         public int Solved { get; set; }
 
-        /// <inheritdoc cref="QueryHistory.QueryWorkerHistories"/>
+        /// <summary>
+        /// Query history of each crawler.
+        /// </summary>
         public ICollection<QueryWorkerHistoryDto> QueryWorkerHistories { get; set; }
     }
 }
