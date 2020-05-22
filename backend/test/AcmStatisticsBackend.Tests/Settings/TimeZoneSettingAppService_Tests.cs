6579ef383db1b4b8b7b843ae033e09e8c7784df5
Liu233w <Liu233w@users.noreply.github.com>
2020-05-22 22:05:15 +1000

    refactor(backend): Merge time zone settings
    Move them to TimeZoneSettingAppService

--- backend/test/AcmStatisticsBackend.Tests/Settings/TimeZoneSettingAppService_Tests.cs
+++ backend/test/AcmStatisticsBackend.Tests/Settings/TimeZoneSettingAppService_Tests.cs
@@ -11,36 +11,32 @@ using Xunit;
 
 namespace AcmStatisticsBackend.Tests.Settings
 {
-    public class SettingAppService_Tests : AcmStatisticsBackendTestBase
+    public class TimeZoneSettingAppService_Tests : AcmStatisticsBackendTestBase
     {
-        private readonly ISettingAppService _settingAppService;
-        private readonly UserTimeZoneManager _userTimeZoneManager;
+        private readonly ITimeZoneSettingAppService _timeZoneSettingAppService;
         private readonly TestClockProvider _clockProvider;
 
-        public SettingAppService_Tests()
+        public TimeZoneSettingAppService_Tests()
         {
             _clockProvider = new TestClockProvider();
-            _settingAppService = Resolve<SettingAppService>(new
+            _timeZoneSettingAppService = Resolve<TimeZoneSettingAppService>(new
             {
                 clockProvider = _clockProvider,
             });
-            _userTimeZoneManager = Resolve<UserTimeZoneManager>();
         }
 
         [Fact]
         public async Task GetUserTimeZone_ShouldWork()
         {
             // arrange
-            await UsingDbContextAsync(async ctx =>
+            LoginAsHostAdmin();
+            await _timeZoneSettingAppService.SetUserTimeZone(new UserTimeZoneDto
             {
-                await _userTimeZoneManager.SetTimeZoneOfUserAsync(
-                    GetHostAdmin().ToUserIdentifier(), "US Eastern Standard Time");
+                TimeZone = "US Eastern Standard Time",
             });
 
-            LoginAsHostAdmin();
-
             // act
-            var result = await _settingAppService.GetUserTimeZone();
+            var result = await _timeZoneSettingAppService.GetUserTimeZone();
 
             // assert
             result.ShouldEqualInJson(new UserTimeZoneDto
@@ -50,11 +46,27 @@ namespace AcmStatisticsBackend.Tests.Settings
         }
 
         [Fact]
-        public async Task SetUserTimeZone_ShouldThrowWhenTimeZoneIsInvalid()
+        public async Task GetUserTimeZone_WhenNoTimeZoneSet_ShouldReturnDefaultTimeZone()
+        {
+            // arrange
+            LoginAsHostAdmin();
+
+            // act
+            var result = await _timeZoneSettingAppService.GetUserTimeZone();
+
+            // assert
+            result.ShouldEqualInJson(new UserTimeZoneDto
+            {
+                TimeZone = "UTC",
+            });
+        }
+
+        [Fact]
+        public async Task SetUserTimeZone_WhenTimeZoneIsInvalid_ShouldThrow()
         {
             LoginAsHostAdmin();
 
-            await _settingAppService.SetUserTimeZone(new UserTimeZoneDto
+            await _timeZoneSettingAppService.SetUserTimeZone(new UserTimeZoneDto
             {
                 TimeZone = "BlaBla",
             }).ShouldThrowAsync<AbpValidationException>();
@@ -67,19 +79,14 @@ namespace AcmStatisticsBackend.Tests.Settings
             LoginAsHostAdmin();
 
             // act
-            await _settingAppService.SetUserTimeZone(new UserTimeZoneDto
+            await _timeZoneSettingAppService.SetUserTimeZone(new UserTimeZoneDto
             {
                 TimeZone = "US Eastern Standard Time",
             });
 
             // assert
-            await UsingDbContextAsync(async ctx =>
-            {
-                var timeZoneOfUser =
-                    await _userTimeZoneManager.GetTimeZoneOfUserAsync(GetHostAdmin().ToUserIdentifier());
-
-                timeZoneOfUser.Id.ShouldBe("US Eastern Standard Time");
-            });
+            var timeZoneOfUser = await _timeZoneSettingAppService.GetUserTimeZone();
+            timeZoneOfUser.TimeZone.ShouldBe("US Eastern Standard Time");
         }
 
         [Fact]
@@ -87,7 +94,7 @@ namespace AcmStatisticsBackend.Tests.Settings
         {
             _clockProvider.Now = new DateTime(2020, 4, 1, 0, 0, 0);
 
-            await _settingAppService.SetUserTimeZone(new UserTimeZoneDto
+            await _timeZoneSettingAppService.SetUserTimeZone(new UserTimeZoneDto
             {
                 // GMT +01:00
                 TimeZone = "Central Europe Standard Time",
@@ -97,7 +104,7 @@ namespace AcmStatisticsBackend.Tests.Settings
             _clockProvider.Now = _clockProvider.Now.AddHours(23);
 
             // should not set time zone
-            await _settingAppService.SetUserTimeZone(new UserTimeZoneDto
+            await _timeZoneSettingAppService.SetUserTimeZone(new UserTimeZoneDto
             {
                 TimeZone = "Dateline Standard Time",
             }).ShouldThrowAsync<UserFriendlyException>();
@@ -106,18 +113,13 @@ namespace AcmStatisticsBackend.Tests.Settings
             _clockProvider.Now = _clockProvider.Now.AddHours(1);
 
             // Can set time zone
-            await _settingAppService.SetUserTimeZone(new UserTimeZoneDto
+            await _timeZoneSettingAppService.SetUserTimeZone(new UserTimeZoneDto
             {
                 TimeZone = "Dateline Standard Time",
             });
 
-            await UsingDbContextAsync(async ctx =>
-            {
-                var timeZoneOfUser =
-                    await _userTimeZoneManager.GetTimeZoneOfUserAsync(GetHostAdmin().ToUserIdentifier());
-
-                timeZoneOfUser.Id.ShouldBe("Dateline Standard Time");
-            });
+            var timeZoneOfUser = await _timeZoneSettingAppService.GetUserTimeZone();
+            timeZoneOfUser.TimeZone.ShouldBe("Dateline Standard Time");
         }
     }
 }
