557aa9a0bfaf9350c0ff477cacd15854cc271fef
Liu233w <Liu233w@users.noreply.github.com>
2020-05-23 00:43:54 +1000

    feat(frontend): add change time zone settings

---
+++ frontend/components/ResultOverlay.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-overlay
+    :absolute="true"
+    :value="value"
+  >
+    <v-chip
+      :color="value.color"
+      v-if="value"
+      @click="$emit('click')"
+    >
+      {{ value.message }}
+    </v-chip>
+  </v-overlay>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      // { color: String, message: String }
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
