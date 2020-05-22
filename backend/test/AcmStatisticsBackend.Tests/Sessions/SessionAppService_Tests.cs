8d728ee2532ef82a65601d11fc9b9db8934f90f0
Liu233w <Liu233w@users.noreply.github.com>
2020-05-22 23:33:32 +1000

    test(backend): remove unnecessary tests

--- backend/test/AcmStatisticsBackend.Tests/Sessions/SessionAppService_Tests.cs
+++ backend/test/AcmStatisticsBackend.Tests/Sessions/SessionAppService_Tests.cs
@@ -14,24 +14,6 @@ namespace AcmStatisticsBackend.Tests.Sessions
             _sessionAppService = Resolve<ISessionAppService>();
         }
 
-        [MultiTenantFact]
-        public async Task Should_Get_Current_User_When_Logged_In_As_Host()
-        {
-            // Arrange
-            LoginAsHostAdmin();
-
-            // Act
-            var output = await _sessionAppService.GetCurrentLoginInformations();
-
-            // Assert
-            var currentUser = await GetCurrentUserAsync();
-            output.User.ShouldNotBe(null);
-            output.User.Name.ShouldBe(currentUser.Name);
-            output.User.Surname.ShouldBe(currentUser.Surname);
-
-            output.Tenant.ShouldBe(null);
-        }
-
         [Fact]
         public async Task Should_Get_Current_User_And_Tenant_When_Logged_In_As_Tenant()
         {
