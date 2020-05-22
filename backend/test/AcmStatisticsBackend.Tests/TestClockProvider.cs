abfcce52de286af1965b7f67871deae476197966
Liu233w <Liu233w@users.noreply.github.com>
2020-05-22 23:13:11 +1000

    test(backend/TestClockProvider): make DateTime UTC

--- backend/test/AcmStatisticsBackend.Tests/TestClockProvider.cs
+++ backend/test/AcmStatisticsBackend.Tests/TestClockProvider.cs
@@ -3,14 +3,23 @@ using Abp.Timing;
 
 namespace AcmStatisticsBackend.Tests
 {
-    internal class TestClockProvider : IClockProvider
+    /// <summary>
+    /// Used to mock time. When setting Now, its Kind will be set to UTC.
+    /// </summary>
+    public class TestClockProvider : IClockProvider
     {
+        private DateTime _now;
+
         public DateTime Normalize(DateTime dateTime)
         {
             return ClockProviders.Utc.Normalize(dateTime);
         }
 
-        public DateTime Now { get; set; }
+        public DateTime Now
+        {
+            get => _now;
+            set => _now = DateTime.SpecifyKind(value, DateTimeKind.Utc);
+        }
 
         public DateTimeKind Kind => DateTimeKind.Utc;
 
