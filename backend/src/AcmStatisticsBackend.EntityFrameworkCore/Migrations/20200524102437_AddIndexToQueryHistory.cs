3d6402c5db86989402644c68b113eac8553d734a
Liu233w <Liu233w@users.noreply.github.com>
2020-05-24 20:25:43 +1000

    perf(backend): add index to QueryHistory

---
+++ backend/src/AcmStatisticsBackend.EntityFrameworkCore/Migrations/20200524102437_AddIndexToQueryHistory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcmStatisticsBackend.Migrations
+{
+    public partial class AddIndexToQueryHistory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_QueryHistories_QuerySummaryId",
+                table: "QueryHistories",
+                column: "QuerySummaryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_QueryHistories_QuerySummaryId",
+                table: "QueryHistories");
+        }
+    }
+}
