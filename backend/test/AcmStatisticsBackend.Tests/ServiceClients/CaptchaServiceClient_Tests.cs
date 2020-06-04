1d501f70080c9540c9f064186d1befbe9ee23362
Liu233w <Liu233w@users.noreply.github.com>
2020-06-04 19:01:09 +1000

    test(backend): add tests of service clients

---
+++ backend/test/AcmStatisticsBackend.Tests/ServiceClients/CaptchaServiceClient_Tests.cs
@@ -0,0 +1,74 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using AcmStatisticsBackend.ServiceClients;
+using FluentAssertions;
+using Flurl.Http.Testing;
+using Xunit;
+
+namespace AcmStatisticsBackend.Tests.ServiceClients
+{
+    public class CaptchaServiceClient_Tests
+    {
+        private readonly ICaptchaServiceClient _captchaServiceClient;
+
+        public CaptchaServiceClient_Tests()
+        {
+            _captchaServiceClient = new CaptchaServiceClient();
+        }
+
+        [Fact]
+        public async Task WhenHttpResponseWithoutError_ShouldWorkCorrectly()
+        {
+            // arrange
+            using var httpTest = new HttpTest();
+            httpTest.RespondWithJson(new
+            {
+                error = false,
+            });
+
+            // act
+            var result = await _captchaServiceClient.ValidateAsync("id", "text");
+
+            // assert
+            httpTest.ShouldHaveCalled("http://captcha-service/api/captcha-service/validate")
+                .WithVerb(HttpMethod.Post)
+                .WithRequestUrlEncoded(new
+                {
+                    id = "id",
+                    text = "text",
+                })
+                .Times(1);
+
+            result.Correct.Should().BeTrue();
+            result.ErrorMessage.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task WhenHttpResponseError_ShouldReportError()
+        {
+            // arrange
+            using var httpTest = new HttpTest();
+            httpTest.RespondWithJson(new
+            {
+                error = true,
+                message = "err message",
+            }, 400);
+
+            // act
+            var result = await _captchaServiceClient.ValidateAsync("id", "text");
+
+            // assert
+            httpTest.ShouldHaveCalled("http://captcha-service/api/captcha-service/validate")
+                .WithVerb(HttpMethod.Post)
+                .WithRequestUrlEncoded(new
+                {
+                    id = "id",
+                    text = "text",
+                })
+                .Times(1);
+
+            result.Correct.Should().BeFalse();
+            result.ErrorMessage.Should().Be("err message");
+        }
+    }
+}
