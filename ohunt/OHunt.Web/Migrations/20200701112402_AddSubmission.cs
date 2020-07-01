eae0fe2236471922d16ebb6199e4738e05fd9c70
Liu233w <Liu233w@users.noreply.github.com>
2020-07-01 21:39:58 +1000

    feat(ohunt): migrate database

---
+++ ohunt/OHunt.Web/Migrations/20200701112402_AddSubmission.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OHunt.Web.Migrations
+{
+    public partial class AddSubmission : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProblemLabel",
+                table: "submissions",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "submissions",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Time",
+                table: "submissions",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "submissions",
+                maxLength: 75,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProblemLabel",
+                table: "submissions");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "submissions");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "submissions");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "submissions");
+        }
+    }
+}
