80133d58034a00bc1228db0474f02f0be4c52357
Liu233w <Liu233w@users.noreply.github.com>
2020-07-01 16:36:49 +1000

    feat(ohunt): use MySqlBulkLoader to batch insert

---
+++ ohunt/OHunt.Web/Utils/TempFile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace OHunt.Web.Utils
+{
+    /// <summary>
+    /// Disposable temporary file
+    ///
+    /// from https://stackoverflow.com/a/400391
+    /// </summary>
+    public sealed class TempFile : IDisposable
+    {
+        private string? _path;
+
+        public TempFile() : this(System.IO.Path.GetTempFileName())
+        {
+        }
+
+        public TempFile(string path)
+        {
+            _path = path;
+        }
+
+        public string Path
+        {
+            get
+            {
+                if (_path == null) throw new ObjectDisposedException(GetType().Name);
+                return _path;
+            }
+        }
+
+        ~TempFile()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                GC.SuppressFinalize(this);
+            }
+
+            if (_path != null)
+            {
+                try
+                {
+                    File.Delete(_path);
+                }
+                catch
+                {
+                } // best effort
+
+                _path = null;
+            }
+        }
+    }
+}
