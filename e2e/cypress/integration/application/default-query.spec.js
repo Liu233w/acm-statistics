a57065b81a1995c0c2f7c336da966ecb1a578427
Liu233w <Liu233w@users.noreply.github.com>
2020-07-10 22:58:29 +1000

    test(e2e): update e2e tests
    + add tests about history
    * fix existing tests

--- e2e/cypress/integration/application/default-query.spec.js
+++ e2e/cypress/integration/application/default-query.spec.js
@@ -7,21 +7,21 @@ describe('Saving default query to cookie when user does not login.', () => {
     cy.visit('/statistics')
 
     cy.get('.v-input:contains("Set all usernames") input').should('have.value', '')
-    cy.get('.worker-item:contains("CodeForces") input').should('have.value', '')
+    cy.get('.worker:contains("CodeForces") input').should('have.value', '')
 
     cy.get('.v-input:contains("Set all usernames") input').type('main-name')
     // wait for other workers to update
-    cy.get('.worker-item:contains("CodeForces") input').should('have.value', 'main-name')
-    cy.get('.worker-item:contains("HDU") input').clear().type('user1')
-    cy.get('.worker-item:contains("POJ") input').clear()
+    cy.get('.worker:contains("CodeForces") input').should('have.value', 'main-name')
+    cy.get('.worker:contains("HDU") input').clear().type('user1')
+    cy.get('.worker:contains("POJ") input').clear()
 
     // act
     cy.contains('save username').click()
     cy.contains('Success!')
 
     cy.reload()
-    cy.get('.worker-item:contains("HDU") input').should('have.value', 'user1')
-    cy.get('.worker-item:contains("POJ") input').should('have.value', '')
+    cy.get('.worker:contains("HDU") input').should('have.value', 'user1')
+    cy.get('.worker:contains("POJ") input').should('have.value', '')
     cy.get('.v-input:contains("Set all usernames") input').should('have.value', 'main-name')
   })
 })
@@ -42,13 +42,13 @@ describe('Saving usernames to server when user has logined.', () => {
     cy.visit('/statistics')
 
     cy.get('.v-input:contains("Set all usernames") input').should('have.value', '')
-    cy.get('.worker-item:contains("CodeForces") input').should('have.value', '')
+    cy.get('.worker:contains("CodeForces") input').should('have.value', '')
 
     cy.get('.v-input:contains("Set all usernames") input').type('main-name')
     // wait for other worker to update
-    cy.get('.worker-item:contains("CodeForces") input').should('have.value', 'main-name')
-    cy.get('.worker-item:contains("HDU") input').clear().type('user1')
-    cy.get('.worker-item:contains("POJ") input').clear()
+    cy.get('.worker:contains("CodeForces") input').should('have.value', 'main-name')
+    cy.get('.worker:contains("HDU") input').clear().type('user1')
+    cy.get('.worker:contains("POJ") input').clear()
 
     cy.contains('save username').click()
     cy.contains('Success!')
@@ -58,10 +58,10 @@ describe('Saving usernames to server when user has logined.', () => {
     cy.login(username)
 
     cy.visit('/statistics')
-    cy.get('.worker-item:contains("HDU") input').should('have.value', 'user1')
-    cy.get('.worker-item:contains("POJ") input').should('have.value', '')
+    cy.get('.worker:contains("HDU") input').should('have.value', 'user1')
+    cy.get('.worker:contains("POJ") input').should('have.value', '')
     cy.get('.v-input:contains("Set all usernames") input').should('have.value', 'main-name')
 
-    cy.get('.worker-item:contains("CodeForces") input').should('have.value', 'main-name')
+    cy.get('.worker:contains("CodeForces") input').should('have.value', 'main-name')
   })
 })
