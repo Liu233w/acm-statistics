d6ac623eb971640a7126272a46d284dc5a50519f
Liu233w <Liu233w@users.noreply.github.com>
2020-07-11 23:19:34 +1000

    feat(backend): add settings AutoSaveHistory

--- backend/src/AcmStatisticsBackend.Core/Configuration/AppSettingNames.cs
+++ backend/src/AcmStatisticsBackend.Core/Configuration/AppSettingNames.cs
@@ -3,5 +3,7 @@
     public static class AppSettingNames
     {
         public const string UiTheme = "App.UiTheme";
+
+        public const string AutoSaveHistory = "App.AutoSaveHistory";
     }
 }
--- backend/src/AcmStatisticsBackend.Core/Configuration/AppSettingProvider.cs
+++ backend/src/AcmStatisticsBackend.Core/Configuration/AppSettingProvider.cs
@@ -9,7 +9,12 @@ namespace AcmStatisticsBackend.Configuration
         {
             return new[]
             {
-                new SettingDefinition(AppSettingNames.UiTheme, "red", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, isVisibleToClients: true),
+                new SettingDefinition(AppSettingNames.UiTheme, "red",
+                    scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User,
+                    isVisibleToClients: true),
+
+                new SettingDefinition(AppSettingNames.AutoSaveHistory, "true", scopes: SettingScopes.User,
+                    isVisibleToClients: true),
             };
         }
     }
