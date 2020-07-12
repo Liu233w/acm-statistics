a2a05b5cea28ee64409494e9b568971f1db1dc3d
Liu233w <Liu233w@users.noreply.github.com>
2020-07-12 15:20:05 +1000

    refactor(frontend): use UserConfig

--- frontend/store/session.js
+++ frontend/store/session.js
@@ -1,8 +1,10 @@
 import Cookies from 'js-cookie'
+import _ from 'lodash'
 
 export const state = () => ({
   login: false,
   username: '',
+  settings: {},
 })
 
 export const mutations = {
@@ -14,17 +16,25 @@ export const mutations = {
     state.login = false
     state.username = ''
   },
+  updateSettings(state, newSettings) {
+    state.settings = _.assign(state.settings, newSettings)
+  },
+  resetSettings(state) {
+    state.settings = {}
+  },
 }
 
 export const actions = {
-  async refreshUser({ commit }) {
+  async refreshUser({ commit, dispatch }) {
     try {
       const res = await this.$axios.$get('/api/services/app/Session/GetCurrentLoginInformations')
 
       if (res.result.user) {
         commit('setUser', { username: res.result.user.userName })
+        await dispatch('refreshSettings')
       } else {
         commit('removeUser')
+        commit('resetSettings')
       }
     } catch (err) {
       Cookies.remove('OAuthToken')
@@ -44,9 +54,15 @@ export const actions = {
     Cookies.set('OAuthToken', res.result.accessToken, config)
 
     await dispatch('refreshUser')
+    await dispatch('refreshSettings')
   },
   async logout({ commit }) {
     Cookies.remove('OAuthToken')
     commit('removeUser')
+    commit('resetSettings')
+  },
+  async refreshSettings({ commit }) {
+    const res = await this.$axios.$get('/api/services/app/UserConfig/GetUserSettings')
+    commit('updateSettings', res.result)
   },
 }
