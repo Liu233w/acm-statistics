1f0986774527ea14c56faf43191b6e650d63502b
Liu233w <Liu233w@users.noreply.github.com>
2020-07-14 15:21:07 +1000

    test(e2e): add tests about auto save history
    resolve to #941

---
+++ e2e/cypress/integration/application/auto-save-history.spec.js
@@ -0,0 +1,94 @@
+Cypress.config('baseUrl', 'http://localhost:3000')
+
+beforeEach(() => {
+  cy.server()
+  cy.route('https://acm-statistics-cors.herokuapp.com/http://acm.hdu.edu.cn/userstatus.php?user=wwwlsmcom',
+    'fixture:summary_hdu.txt')
+    .as('summary_hdu')
+  cy.route('POST', '/api/services/app/QueryHistory/SaveOrReplaceQueryHistory')
+    .as('save-history')
+})
+
+function specs(enterStatistics) {
+
+  describe('at default', () => {
+
+    it('saves history automatically', () => {
+      cy.registerAndGetUsername()
+      enterStatisticsAndQuery(enterStatistics)
+      cy.wait('@save-history')
+      cy.log('assert')
+      cy.visit('/history')
+      cy.get('i.mdi-delete').should('have.length', 1)
+    })
+  })
+
+  describe('set settings to not save history', () => {
+
+    let username
+
+    before(() => {
+      cy.registerAndGetUsername().then(u => username = u)
+      cy.clearCookies()
+    })
+
+    it('should change settings successfully', () => {
+      cy.login(username)
+      cy.visit('/settings')
+      cy.contains('Auto save query history').parents('.v-card').within(() => {
+        cy.get('input[value="false"]').click({ force: true })
+        cy.get('button').contains('save', { matchCase: false }).click()
+        cy.matchImageSnapshot()
+      })
+    })
+
+    it('should not auto save history', () => {
+      cy.login(username)
+      enterStatisticsAndQuery(enterStatistics)
+      cy.log('assert')
+      cy.visit('/history')
+      cy.get('i.mdi-delete').should('have.length', 0)
+    })
+
+    it('should go to summary when click view summary', () => {
+      cy.login(username)
+      enterStatisticsAndQuery(enterStatistics)
+      cy.contains('/ submission', { matchCase: false }).click()
+      cy.location('pathname').should('satisfy', val => val.startsWith('/history/'))
+    })
+
+    it('should have one record in history', () => {
+      cy.login(username)
+      cy.visit('/history')
+      cy.get('i.mdi-delete').should('have.length', 1)
+    })
+  })
+
+}
+
+describe('when directly enter statistics page', () => specs(() => {
+  cy.visit('/statistics')
+}))
+
+describe('when enter statistics page from other page', () => specs(() => {
+  cy.visit('/about')
+  cy.get('i.mdi-menu').parents('button').click()
+  cy.contains('statistics', { matchCase: false }).click()
+}))
+
+function enterStatisticsAndQuery(enterFunc) {
+
+  cy.log('save a history')
+  enterFunc()
+
+  cy.get('div[title="HDU"]').parents('.worker').within(() => {
+
+    cy.get('div:contains("Username") input').type('wwwlsmcom')
+
+    cy.get('button:contains("refresh")').click()
+    cy.wait('@summary_hdu')
+
+    cy.contains('34')
+    cy.contains('72')
+  })
+}
