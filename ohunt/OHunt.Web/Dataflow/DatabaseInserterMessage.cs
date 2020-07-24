65c5df559be48c1426e5bdc4016a4156ae448201
Liu233w <Liu233w@users.noreply.github.com>
2020-07-24 21:58:01 +1000

    refactor(ohunt): use factory methods of messages

--- ohunt/OHunt.Web/Dataflow/DatabaseInserterMessage.cs
+++ ohunt/OHunt.Web/Dataflow/DatabaseInserterMessage.cs
@@ -4,15 +4,38 @@ namespace OHunt.Web.Dataflow
     /// A message sent to database inserter
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public struct DatabaseInserterMessage<T>
+    public readonly struct DatabaseInserterMessage<T>
         where T : class
     {
-        public T? Entity { get; set; }
+        private DatabaseInserterMessage(T? entity, bool forceInsert)
+        {
+            Entity = entity;
+            ForceInsert = forceInsert;
+        }
+
+        /// <summary>
+        /// Create an instance with the entity
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="forceInsert">Should it trigger force insert (default false)</param>
+        /// <returns></returns>
+        public static DatabaseInserterMessage<T> OfEntity(T entity, bool forceInsert = false)
+        {
+            return new DatabaseInserterMessage<T>(entity, forceInsert);
+        }
+
+        /// <summary>
+        /// Create an instance with force insert
+        /// </summary>
+        public static DatabaseInserterMessage<T> ForceInsertMessage
+            => new DatabaseInserterMessage<T>(null, true);
+
+        public T? Entity { get; }
 
         /// <summary>
         /// If it is true, all data in buffer is inserted to database
         /// even if the buffer is not full.
         /// </summary>
-        public bool ForceInsert { get; set; }
+        public bool ForceInsert { get; }
     }
 }
