dbfb0a588d8a4dd142ad836e9ca362b97a3c4a96
Liu233w <Liu233w@users.noreply.github.com>
2020-07-26 19:55:54 +1000

    refactor(ohunt): refactor crawler 2
    pipeline is not done yet

--- ohunt/OHunt.Web/Schedule/ScheduleCrawlerService.cs
+++ ohunt/OHunt.Web/Schedule/ScheduleCrawlerService.cs
@@ -12,15 +12,10 @@ namespace OHunt.Web.Schedule
     public class ScheduleCrawlerService : IHostedService, IDisposable
     {
         private readonly ILogger<ScheduleCrawlerService> _logger;
-        private Timer? _timer;
+        private Timer _timer = null!;
         private readonly SubmissionCrawlerCoordinator _coordinator;
 
         private readonly ZojSubmissionCrawler _zojSubmissionCrawler;
-
-        private volatile int _isRunning = 0;
-        private volatile Task[]? _runningTask = null;
-
-        private volatile CancellationTokenSource _tokenSource = new CancellationTokenSource();
 
         public ScheduleCrawlerService(
             ILogger<ScheduleCrawlerService> logger,
@@ -34,6 +29,10 @@ namespace OHunt.Web.Schedule
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
+            _coordinator.Initialize(new[]
+            {
+                _zojSubmissionCrawler,
+            });
             _timer = new Timer(DoWork, null, TimeSpan.Zero,
                 TimeSpan.FromHours(0.5));
 
@@ -45,11 +44,7 @@ namespace OHunt.Web.Schedule
             _logger.LogInformation("Timed Hosted Service is stopping.");
 
             _timer?.Change(Timeout.Infinite, 0);
-            _tokenSource.Cancel();
-
-            return _runningTask != null
-                ? Task.WhenAll(_runningTask)
-                : Task.CompletedTask;
+            return _coordinator.Cancel();
         }
 
         public void Dispose()
@@ -59,29 +54,8 @@ namespace OHunt.Web.Schedule
 
         private void DoWork(object? state)
         {
-            _logger.LogTrace("Starting crawler schedule");
-
-            if (Interlocked.Exchange(ref _isRunning, 1) == 1)
-            {
-                _logger.LogInformation("Previous crawler is not finished yet");
-                return;
-            }
-
-            _runningTask = new Task[]
-            {
-                // _coordinator.WorkAsync(_zojSubmissionCrawler, _tokenSource.Token),
-            };
-            try
-            {
-                Task.WaitAll(_runningTask);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Exception when running worker");
-            }
-
-            _logger.LogTrace("All crawler finished");
-            _isRunning = 0;
+            _logger.LogTrace("Starting all crawlers");
+            _coordinator.StartAllCrawlers();
         }
     }
 }
