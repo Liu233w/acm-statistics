28a89b4d8729d05fdbb24eba0a05b637a75ed8e5
Liu233w <Liu233w@users.noreply.github.com>
2020-07-30 20:58:02 +1000

    test(e2e): add test dialog in /about

--- e2e/cypress/integration/frontend/about.spec.js
+++ e2e/cypress/integration/frontend/about.spec.js
@@ -10,4 +10,12 @@ describe('overall', () => {
     cy.viewport(1920, 1080)
     cy.matchImageSnapshot()
   })
+
+  it('wechat dialog can be rendered correctly', () => {
+    cy.contains('div[role="listitem"]', '微信公众号').click()
+    cy.get('.v-dialog.v-dialog--active').within(() => {
+      cy.contains('微信公众号')
+      cy.matchImageSnapshot()
+    })
+  })
 })
