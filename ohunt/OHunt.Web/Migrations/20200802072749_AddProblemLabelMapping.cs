be9d20dc70fcb8d7c0c24c63d33b82c9a60b96cc
Liu233w <Liu233w@users.noreply.github.com>
2020-08-02 17:41:51 +1000

    feat(ohunt): add db migration

---
+++ ohunt/OHunt.Web/Migrations/20200802072749_AddProblemLabelMapping.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OHunt.Web.Migrations
+{
+    public partial class AddProblemLabelMapping : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "problem_label_mappings",
+                columns: table => new
+                {
+                    ProblemId = table.Column<long>(nullable: false),
+                    OnlineJudgeId = table.Column<int>(nullable: false),
+                    ProblemLabel = table.Column<string>(maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_problem_label_mappings", x => new { x.ProblemId, x.OnlineJudgeId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "problem_label_mappings");
+        }
+    }
+}
