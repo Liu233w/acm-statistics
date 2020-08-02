26bf039cea82d0821dd1e1199b66bb6506805846
Liu233w <Liu233w@users.noreply.github.com>
2020-08-02 23:48:25 +1000

    perf(crawler): add host to ohunt request
    * so that it can be used in both frontend and backend

--- crawler/crawlers/uva.js
+++ crawler/crawlers/uva.js
@@ -30,7 +30,7 @@ module.exports = async function (config, username) {
   })
 
   const ohuntRes = await request
-    .post('/api/ohunt/problems/resolve-label')
+    .post('https://new.npuacm.info/api/ohunt/problems/resolve-label')
     .send({
       onlineJudge: 'uva',
       list: [...acSet],
