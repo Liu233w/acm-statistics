119e92e358477581cd8ea1bfece5f7c2ed37b83f
Liu233w <Liu233w@users.noreply.github.com>
2021-01-26 16:38:21 +1100

    test(e2e): remove unneeded default mock

--- e2e/http-mocks/src/preActivation.js
+++ e2e/http-mocks/src/preActivation.js
@@ -2,10 +2,7 @@ const mock = require('./lib/mock')
 
 module.exports = async mocks => {
   const actived = [
-    mocks.busuanzi,
-    mocks.tajs,
     mocks.googleAnalysis,
-    mocks.googleAds,
   ]
 
   for (let item of actived) {
