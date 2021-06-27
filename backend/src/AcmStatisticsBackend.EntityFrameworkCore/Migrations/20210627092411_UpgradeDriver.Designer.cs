d8831f1ba883aa6d1b143c652848385f11a77389
Liu233w <gh@liu233w.com>
2021-06-27 19:28:05 +1000

    build(backend): update migrations

--- backend/src/AcmStatisticsBackend.EntityFrameworkCore/Migrations/20210627092411_UpgradeDriver.Designer.cs
+++ backend/src/AcmStatisticsBackend.EntityFrameworkCore/Migrations/20210627092411_UpgradeDriver.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AcmStatisticsBackend.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AcmStatisticsBackend.Migrations
 {
     [DbContext(typeof(AcmStatisticsBackendDbContext))]
-    partial class AcmStatisticsBackendDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210627092411_UpgradeDriver")]
+    partial class UpgradeDriver
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
