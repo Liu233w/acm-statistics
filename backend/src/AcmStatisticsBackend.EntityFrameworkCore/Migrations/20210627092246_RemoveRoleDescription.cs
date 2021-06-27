ddea502210eb5e988758fc4b33edbf0738319eb2
Liu233w <gh@liu233w.com>
2021-06-27 19:23:38 +1000

    perf(backend): remove role description
    That can cause error

---
+++ backend/src/AcmStatisticsBackend.EntityFrameworkCore/Migrations/20210627092246_RemoveRoleDescription.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcmStatisticsBackend.Migrations
+{
+    public partial class RemoveRoleDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AbpRoles");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AbpRoles",
+                type: "longtext CHARACTER SET utf8mb4",
+                maxLength: 5000,
+                nullable: true);
+        }
+    }
+}
