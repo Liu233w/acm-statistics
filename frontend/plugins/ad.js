c14b6834ef2f2056748bc7024ff023fcafaf1e16
Liu233w <gh@liu233w.com>
2021-08-24 19:46:00 +1000

    feat(frontend): remove google analysis
    For privacy!

--- frontend/plugins/ad.js
+++
@@ -1,7 +0,0 @@
-export default function () {
-  if (process.client) {
-    window._mNHandle = window._mNHandle || {}
-    window._mNHandle.queue = window._mNHandle.queue || []
-    window.medianet_versionId = '3121199'
-  }
-}
