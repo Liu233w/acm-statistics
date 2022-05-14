88bbcebb3ad628885c1fce99b5022f423643f9ba
Liu233w <gh@liu233w.com>
2022-05-14 23:04:33 +1000

    build(frontend): remove apk and npm mirror
    FUCK OFF GFW

--- build/node-base.mk
+++ build/node-base.mk
@@ -3,14 +3,8 @@
 include share.mk
 
 NodeBaseOption =
-ifdef APK_MIRROR
-NodeBaseOption := --build-arg APK_MIRROR=$(APK_MIRROR) $(NodeBaseOption)
-endif
 ifdef CODECOV
 NodeBaseOption := --build-arg CODECOV=$(CODECOV) $(NodeBaseOption)
-endif
-ifdef NPM_MIRROR
-NodeBaseOption := --build-arg NPM_MIRROR=$(NPM_MIRROR) $(NodeBaseOption)
 endif
 
 build:
