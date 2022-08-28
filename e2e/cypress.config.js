38a5379a433ca88d00fe665fc9d5c8828d6146a4
Liu233w <gh@liu233w.com>
2022-08-28 21:39:06 +1000

    feat(e2e): update cypress config

---
+++ e2e/cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/integration/**/*.spec.js',
+    setupNodeEvents(on, config) {
+      require('@simonsmith/cypress-image-snapshot/plugin')
+          .addMatchImageSnapshotPlugin(on, config)
+      require('cypress-terminal-report/src/installLogsPrinter')(on)
+    },
+    experimentalSessionAndOrigin: true,
+  },
+
+  retries: {
+      runMode: 2,
+      openMode: 0,
+  },
+  reporter: '@simonsmith/cypress-image-snapshot/reporter',
+  projectId: '4s32o7',
+})
