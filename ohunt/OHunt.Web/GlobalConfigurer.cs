a7dac685a0dc145a819000308aa574866253f44c
Shumin Liu <shumin.liu@tngtech.com>
2025-08-12 21:18:42 +1000

    build(ohunt): upgrade dotnet to 8.0

--- ohunt/OHunt.Web/GlobalConfigurer.cs
+++ ohunt/OHunt.Web/GlobalConfigurer.cs
@@ -1,8 +1,5 @@
-using System.Net.Http;
-using System.Net.Security;
 using System.Runtime.CompilerServices;
 using Flurl.Http;
-using Flurl.Http.Configuration;
 
 namespace OHunt.Web
 {
@@ -14,17 +11,8 @@ namespace OHunt.Web
         [ModuleInitializer]
         public static void Configure()
         {
-            FlurlHttp.ConfigureClient("https://icpcarchive.ecs.baylor.edu/uhunt", cli =>
-                cli.Settings.HttpClientFactory = new UntrustedCertClientFactory());
-        }
-    }
-    
-    public class UntrustedCertClientFactory : DefaultHttpClientFactory
-    {
-        public override HttpMessageHandler CreateMessageHandler() {
-            return new HttpClientHandler {
-                ServerCertificateCustomValidationCallback = (a, b, c, d) => true
-            };
+            FlurlHttp.ConfigureClientForUrl("https://icpcarchive.ecs.baylor.edu/uhunt")
+                .ConfigureInnerHandler(handler => handler.ServerCertificateCustomValidationCallback = (msg, cert, chain, errors) => true);
         }
     }
 }
